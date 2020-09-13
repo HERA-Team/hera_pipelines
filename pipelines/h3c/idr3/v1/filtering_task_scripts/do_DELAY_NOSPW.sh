f4d01192f15b7ac50e21e5c0924ac08763432a78
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-09-13 00:07:46 -0600

    add no spw delay.

---
+++ pipelines/h3c/idr3/v1/filtering_task_scripts/do_DELAY_NOSPW.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+set -e
+
+#import common functions
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# Parameters are set in the configuration file, here we define their positions,
+# which must be consistent with the config.
+# 1 - filename
+# 3 - calibration file
+# 4 - extra label for the output file.
+# 5 - tol level to subtract foregrounds too
+# 6 - standoff delay standoff in ns for filtering window.
+# 7 - cache_dir, directory to store cache files in.
+fn="${1}"
+data_ext="${2}"
+calibration="${3}"
+label="${4}"
+tol="${5}"
+standoff="${6}"
+cache_dir="${7}"
+# get julian day from file name
+jd=$(get_jd $fn)
+# generate output file name
+fn_out=${fn%.uvh5}.${label}.foreground_filtered.${data_ext}
+# if cache directory does not exist, make it
+if [ ! -d "${cache_dir}" ]; then
+  mkdir ${cache_dir}
+fi
+
+if [ "${calibration}" != "none" ]
+then
+  calfile=${fn%.uvh5}.${calibration}
+else
+  calfile="none"
+fi
+
+fn_in=${fn%.uvh5}.${label}.${data_ext}
+
+
+echo dayenu_delay_filter_run.py ${fn_in} --calfile ${calfile} \
+  --res_outfilename ${fn_out} --clobber --skip_edge_flags \
+  --tol ${tol} --cache_dir ${cache_dir} --standoff ${standoff} #--write_cache --read_cache
+
+dayenu_delay_filter_run.py ${fn_in} --calfile ${calfile} \
+    --res_outfilename ${fn_out} --clobber --skip_edge_flags \
+    --tol ${tol} --cache_dir ${cache_dir} --standoff ${standoff} #--write_cache --read_cache
