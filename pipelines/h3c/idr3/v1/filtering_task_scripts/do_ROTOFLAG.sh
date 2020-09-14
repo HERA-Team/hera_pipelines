4bfdd79d9f859a98a5edf8bfb5e8a69302b2019b
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-09-14 02:56:26 -0700

    roto flag script.

---
+++ pipelines/h3c/idr3/v1/filtering_task_scripts/do_ROTOFLAG.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+set -e
+
+#import common functions
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# Parameters are set in the configuration file, here we define their positions,
+# which must be consistent with the config.
+# 1 - file name
+# 3 - percentile_freq
+# 4 - percentile_time
+# 5 - niters
+# 6 - output_label
+# 7 - yaml_dir
+# 9 - data extension
+# 10 - kf_size
+# 11 - kt_size
+
+fn="${1}"
+percentile_freq="${2}"
+percentile_time="${3}"
+niters="${4}"
+output_label="${5}"
+data_ext="${6}"
+kf_size="${7}"
+kt_size="${8}"
+
+jd=$(get_jd $fn)
+int_jd=${jd:0:7}
+
+# get xtalk waterfall file
+xtalk_wf=${fn%.uvh5}.${label}.xtalk_filtered_waterfall.${data_ext}
+
+
+
+echo roto_flag_run.py --data_files ${xtalk_wf} \
+                      --flag_percentile_freq ${percentile_freq} \
+                      --flag_percentile_time ${percentile_time} \
+                      --output_label ${label}.roto_flag \
+                      --kf_size ${kf_size} \
+                      --kt_size ${kt_size} \
+                      --niters ${niters}
+
+roto_flag_run.py --data_files ${xtalk_wf} \
+                 --flag_percentile_freq ${percentile_freq} \
+                 --flag_percentile_time ${percentile_time} \
+                 --a_priori_flag_yaml ${a_priori_flag_yaml} \
+                 --output_label ${label}.roto_flag \
+                 --kf_size ${kf_size} \
+                 --kt_size ${kt_size} \
+                 --niters ${niters}
