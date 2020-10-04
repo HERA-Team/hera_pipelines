b37a9d3677e0b11cfc33d8770e15de32acd26d9a
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-10-04 13:01:58 -0700

    comment out lines not yet supported by hera_pspec.

--- pipelines/h3c/idr3/v1/filtering_task_scripts/do_PSPEC.sh
+++ pipelines/h3c/idr3/v1/filtering_task_scripts/do_PSPEC.sh
@@ -26,7 +26,7 @@ output=zen.${jd}.${label}.xtalk_filtered_waterfall_noforegrounds_res.uvp
 # form power spectrum between even and odd data sets with offset times.
 
 
-# pspec of residual xtalk waterfall
+# power spectra of data with no foregrounds or xtalk.
 echo pspec_run.py --allow_fft --store_cov_diag\
  --vis_units Jy --cov_model empirical_pspec --overwrite\
  --dset_pairs '0,1' --pol_pairs 'ee ee, nn nn'\
@@ -41,11 +41,10 @@ pspec_run.py --allow_fft --store_cov_diag\
   --time_avg --file_type uvh5 ${even_file} ${odd_file} ${output}\
   --taper bh
 
-
+  # power spectra of data with foregrounds but no xtalk.
 even_file=zen.${jd}.even.${label}.xtalk_filtered_waterfall_withforegrounds_res.${data_ext}
 odd_file=${even_file/even/odd}
 output=zen.${jd}.${label}.xtalk_filtered_waterfall_withforegrounds_res.uvp
-# form power spectrum between even and odd data sets with offset times.
 
 echo pspec_run.py --allow_fft --store_cov_diag\
  --vis_units Jy --cov_model empirical_pspec --overwrite\
@@ -62,11 +61,10 @@ pspec_run.py --allow_fft --store_cov_diag\
   --taper bh
 
 
-
+  # power spectra of data with the foregrounds and xtalk retained -- to estimate signal loss from xtalk filter.
   even_file=zen.${jd}.even.${label}.xtalk_filtered_waterfall_withforegrounds_filled.${data_ext}
   odd_file=${even_file/even/odd}
   output=zen.${jd}.${label}.xtalk_filtered_waterfall_withforegrounds_filled.uvp
-  # form power spectrum between even and odd data sets with offset times.
 
   echo pspec_run.py --allow_fft --store_cov_diag\
    --vis_units Jy --cov_model empirical_pspec --overwrite\
@@ -83,23 +81,26 @@ pspec_run.py --allow_fft --store_cov_diag\
     --taper bh
 
 
+    # power spectra of data with the foregrounds and no xtalk but also with dayenu filter so that
+    # we effectively get no foregrounds or cross-talk (should be same as first case) but we also get accurate window functions.
+    # this isn't ready yet because we don't yet have a streamlined way of setting dayenu filter parameters.
+    # COMMENT OUT FOR NOW!
 
-    even_file=zen.${jd}.even.${label}.xtalk_filtered_waterfall_withforegrounds_res.${data_ext}
-    odd_file=${even_file/even/odd}
-    output=zen.${jd}.${label}.xtalk_filtered_waterfall_withforegrounds_res_dayenu.uvp
-    # form power spectrum between even and odd data sets with offset times.
+    #even_file=zen.${jd}.even.${label}.xtalk_filtered_waterfall_withforegrounds_res.${data_ext}
+    #odd_file=${even_file/even/odd}
+    #output=zen.${jd}.${label}.xtalk_filtered_waterfall_withforegrounds_res_dayenu.uvp
 
-    echo pspec_run.py --allow_fft --store_cov_diag\
-     --vis_units Jy --cov_model empirical_pspec --overwrite\
-     --dset_pairs '0,1' --pol_pairs 'ee ee, nn nn'\
-     --Jy2mK --beam ${beam_file} --interleave_times --sampling\
-     --time_avg --file_type uvh5 ${even_file} ${odd_file} ${output}\
-     --input_data_weight dayenu
+    #echo pspec_run.py --allow_fft --store_cov_diag\
+    # --vis_units Jy --cov_model empirical_pspec --overwrite\
+    # --dset_pairs '0,1' --pol_pairs 'ee ee, nn nn'\
+    # --Jy2mK --beam ${beam_file} --interleave_times --sampling\
+    # --time_avg --file_type uvh5 ${even_file} ${odd_file} ${output}\
+    # --input_data_weight dayenu
 
-    pspec_run.py --allow_fft --store_cov_diag\
-      --vis_units Jy --cov_model empirical_pspec --overwrite\
-      --dset_pairs '0,1' --pol_pairs 'ee ee, nn nn'\
-      --Jy2mK --beam ${beam_file} --interleave_times --sampling\
-      --time_avg --file_type uvh5 ${even_file} ${odd_file} ${output}\
-      --input_data_weight dayenu
-      # form power spectra with dayenu filter
+    #pspec_run.py --allow_fft --store_cov_diag\
+    #  --vis_units Jy --cov_model empirical_pspec --overwrite\
+    #  --dset_pairs '0,1' --pol_pairs 'ee ee, nn nn'\
+    #  --Jy2mK --beam ${beam_file} --interleave_times --sampling\
+    #  --time_avg --file_type uvh5 ${even_file} ${odd_file} ${output}\
+    #  --input_data_weight dayenu
+    # form power spectra with dayenu filter
