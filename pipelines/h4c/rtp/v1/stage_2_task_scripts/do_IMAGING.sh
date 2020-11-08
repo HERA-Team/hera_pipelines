a178961201ae4ebf347eff076c34b896524d0363
Nicholas Kern <nkern@berkeley.edu>
2020-11-08 17:49:13 -0500

    modified:   do_IMAGING.sh

--- pipelines/h4c/rtp/v1/stage_2_task_scripts/do_IMAGING.sh
+++ pipelines/h4c/rtp/v1/stage_2_task_scripts/do_IMAGING.sh
@@ -92,7 +92,7 @@ if [ -f ${model_file} ]; then
     echo rm ${model_file}
     rm ${model_file}
 fi
-if [ -f ${model_file%.uvfits}.ms ]; then
+if [ -d ${model_file%.uvfits}.ms ]; then
     echo rm -r ${model_file%.uvfits}.ms
     rm -r ${model_file%.uvfits}.ms
 fi
@@ -100,7 +100,7 @@ if [ -f ${res_file} ]; then
     echo rm ${res_file}
     rm ${res_file}
 fi
-if [ -f ${res_file%.uvfits}.ms ]; then
+if [ -d ${res_file%.uvfits}.ms ]; then
     echo rm -r ${res_file%.uvfits}.ms
     rm -r ${res_file%.uvfits}.ms
 fi
