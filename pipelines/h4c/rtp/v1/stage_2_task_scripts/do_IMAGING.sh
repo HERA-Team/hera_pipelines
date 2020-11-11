e75c783a692688ee0168b6c8004de70857f1ac68
Josh Dillon <jsdillon@berkeley.edu>
2020-11-10 16:54:57 -0800

    make do_IMAGING more fault tolerant
    if the file is fully flagged, casa won't make a image fits, so this will ignore the next step rather than error

--- pipelines/h4c/rtp/v1/stage_2_task_scripts/do_IMAGING.sh
+++ pipelines/h4c/rtp/v1/stage_2_task_scripts/do_IMAGING.sh
@@ -67,16 +67,14 @@ if [ -f ${res_file} ]; then
 fi
 
 # collect stokpol FITS output
-fits_files=(`ls *spw?.stokpol.image.fits`)
-for ff in "${fits_files[@]}"
+for ff in *spw?.stokpol.image.fits
 do
     # stokes I, Q, U, V
     echo python ${casa_imaging_scripts}/plot_fits.py ${ff} --cmap bone_r,coolwarm,coolwarm,coolwarm --vmin 0,-5,-3,-3 --vmax 10,5,3,3 --radius 20
     python ${casa_imaging_scripts}/plot_fits.py ${ff} --cmap bone_r,coolwarm,coolwarm,coolwarm --vmin 0,-5,-3,-3 --vmax 10,5,3,3 --radius 20
 done
 # collect vispol FITS output
-fits_files=(`ls *spw?.vispol.image.fits`)
-for ff in "${fits_files[@]}"
+for ff in *spw?.vispol.image.fits
 do
     # XX, YY
     echo python ${casa_imaging_scripts}/plot_fits.py ${ff} --cmap bone_r --vmin 0 --vmax 10 --radius 20
