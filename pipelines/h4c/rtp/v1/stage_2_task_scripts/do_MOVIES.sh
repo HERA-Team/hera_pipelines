40f8b0a69a3e2e0b43743fc9a8e324d162de9ca7
Josh Dillon <jsdillon@berkeley.edu>
2020-11-25 11:51:06 -0800

    fix problems with temp directory name and add comments

--- pipelines/h4c/rtp/v1/stage_2_task_scripts/do_MOVIES.sh
+++ pipelines/h4c/rtp/v1/stage_2_task_scripts/do_MOVIES.sh
@@ -29,22 +29,27 @@ commas="${spw//[^,]}"
 for dtype in "." ".model." ".res."; do
     for pol in stokpol vispol; do
         for n in `seq 0 ${nspw}`; do
+            # make temporary directory (replacing periods with underscores) to store images in one place for using ffmpeg
             tempdir=${jd}.${dtype}spw${n}.${pol}_movie_temp
+            tempdir=${tempdir//./_}
+            # make new directory (deleting old if necessary) and cd into it and copy all pngs over
+            rm -rf ${tempdir}
             mkdir ${tempdir}
             cd ${tempdir}
-            for png in *_image/zen.${jd}.*.calibrated${dtype}spw${n}.${pol}.image.png; do
-                echo moving ${png}
+            for png in ../*_image/zen.${jd}.*.calibrated${dtype}spw${n}.${pol}.image.png; do
                 cp ${png} .
             done
 
+            # make movie
             echo ${ffmpeg} -framerate ${framerate} -pattern_type glob -i "./*.png" -s:v ${framesize} \
                 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p zen.${jd}${dtype}spw${n}.${pol}.mp4
             ${ffmpeg} -framerate ${framerate} -pattern_type glob -i "./*.png" -s:v ${framesize} \
                 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p zen.${jd}${dtype}spw${n}.${pol}.mp4
-            
+
+            # return to main analysis directory, move movie, remove tempdir
             cd ..
             mv ${tempdir}/zen.${jd}${dtype}spw${n}.${pol}.mp4 .
             rm -rf ${tempdir}
         done
     done
-done
+
