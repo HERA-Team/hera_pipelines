d84a97b14d2fdc05e8cd2a2b580fe1b59615d4c7
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-01-29 12:15:56 -0800

    fix bugs in auto repairing scritp.

--- utils/fix_autos.sh
+++ utils/fix_autos.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # manual script to repair missing autocorrelations.
 # should be run in folder holding all the .sum files
+sumdiff=("sum" "diff")
 
-for sd in ("sum" "diff")
+for sd in ${sumdiff[@]}
 do
   for fn in *.${sd}.uvh5
   do
       autos_file=${fn/.${sd}.uvh5/.${sd}.autos.uvh5}
-      if ![ -e "${fn}" "]
+      if [ ! -e "${autos_file}" ]
       then
         echo extract_autos.py ${fn} ${autos_file} --clobber
         extract_autos.py ${fn} ${autos_file} --clobber
