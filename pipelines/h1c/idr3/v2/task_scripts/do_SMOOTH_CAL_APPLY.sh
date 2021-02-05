ef286de5b9f56f8eec021f37919f7ff6f8eb28cd
Josh Dillon <jsdillon@berkeley.edu>
2021-02-05 15:46:43 -0800

    create makeflow and script for prepping files for LST binning

---
+++ pipelines/h1c/idr3/v2/task_scripts/do_SMOOTH_CAL_APPLY.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# import common functions
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# Positional parameters passed as defined in the configuration file:
+# 1 - base filename
+fn=${1}
+outfile=${fn%.xx.HH.uv}.sum.smooth_calibrated.uvh5
+sc_file=${fn%.xx.HH.uv}.sum.smooth_abs.calfits
+
+echo apply_cal.py ${fn} ${outfile} --new_cal ${sc_file} --vis_units Jy --clobber 
+apply_cal.py ${fn} ${outfile} --new_cal ${sc_file} --vis_units Jy --clobber 
