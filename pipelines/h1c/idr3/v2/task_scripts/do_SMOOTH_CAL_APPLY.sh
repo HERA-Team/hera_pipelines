3c664b7dcd3b8927b8e8c17287c043cb692822a7
Josh Dillon <jsdillon@berkeley.edu>
2021-02-05 16:37:44 -0800

    fix script to use uvh5 file

--- pipelines/h1c/idr3/v2/task_scripts/do_SMOOTH_CAL_APPLY.sh
+++ pipelines/h1c/idr3/v2/task_scripts/do_SMOOTH_CAL_APPLY.sh
@@ -8,8 +8,9 @@ source ${src_dir}/_common.sh
 # Positional parameters passed as defined in the configuration file:
 # 1 - base filename
 fn=${1}
+infile=${fn%.xx.HH.uv}.sum.uvh5
 outfile=${fn%.xx.HH.uv}.sum.smooth_calibrated.uvh5
 sc_file=${fn%.xx.HH.uv}.sum.smooth_abs.calfits
 
-echo apply_cal.py ${fn} ${outfile} --new_cal ${sc_file} --vis_units Jy --clobber 
-apply_cal.py ${fn} ${outfile} --new_cal ${sc_file} --vis_units Jy --clobber 
+echo apply_cal.py ${infile} ${outfile} --new_cal ${sc_file} --vis_units Jy --clobber 
+apply_cal.py ${infile} ${outfile} --new_cal ${sc_file} --vis_units Jy --clobber 
