31e8281e300c289f761998c199733f6770dc87a2
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-02-16 10:49:41 -0800

    make sure to reset inputargs.

--- pipelines/h4c/post_processing/v2/scripts/lstbin_days.sh
+++ pipelines/h4c/post_processing/v2/scripts/lstbin_days.sh
@@ -24,12 +24,14 @@ do
   do
     if [ "${systematics}" == "after" ]
     then
+      inputargs=""
       for str in ${inputdirs[@]}
       do
         inputargs=${inputargs}" '${str}/zen.*.${sd}.${label}.xtalk_filtered_res.smooth_avg_vis.${parity}.uvh5'"
       done
       echo lstbin_run.py${inputargs} --outdir ${outputdir} --rephase --average_redundant_baselines --overwrite --file_ext "{type}.{time:7.5f}.${sd}.${label}.xtalk_filtered.smooth_avg_vis.${parity}.uvh5"
       lstbin_run.py${inputargs} --outdir ${outputdir} --rephase --average_redundant_baselines --overwrite --file_ext "{type}.{time:7.5f}.${sd}.${label}.xtalk_filtered.smooth_avg_vis.${parity}.uvh5"
+      inputargs=""
       for str in ${inputdirs[@]}
       do
         inputargs=${inputargs}" '${str}/zen.*.${sd}.${label}.fg_filtered_res.smooth_avg_vis.${parity}.uvh5'"
@@ -39,6 +41,7 @@ do
 
     elif [ "${systematics}" == "before" ]
     then
+      inputargs=""
       for str in ${inputdirs[@]}
       do
         inputargs=${inputargs}" '${str}/zen.*.${sd}.${label}.chunked.smooth_avg_vis.${parity}.uvh5'"
