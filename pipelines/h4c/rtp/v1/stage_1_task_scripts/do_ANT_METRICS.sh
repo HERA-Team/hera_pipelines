39f5c86bc4a40668a80568b5bf2a37fab7daf1f4
Josh Dillon <jsdillon@berkeley.edu>
2021-02-17 13:42:37 -0800

    remove statuses and apriori xants

--- pipelines/h4c/rtp/v1/stage_1_task_scripts/do_ANT_METRICS.sh
+++ pipelines/h4c/rtp/v1/stage_1_task_scripts/do_ANT_METRICS.sh
@@ -9,33 +9,27 @@ source ${src_dir}/_common.sh
 
 # Parameters are set in the configuration file. Here we define their positions,
 # which must be consistent with the config.
-# 1 - crossCut: Modified z-score cut for most cross-polarized antenna.
-# 2 - deadCut: Modified z-score cut for most likely dead antenna.
+# 1 - crossCut: Metric cut for most cross-polarized antenna.
+# 2 - deadCut: Metric cut for most likely dead antenna.
 # 3 - Nbls_per_load: Number of baselines to load simultaneously.
 # 4 - extension: Extension to be appended to the file name.
-# 5 - all_ants_statuses: string list of comma-separated (no spaces) antenna statuses worth running ant_metrics on
-# 6 - upload_to_librarian: global boolean trigger
-# 7 - librarian_ant_metrics: boolean trigger for this step
-# 8+ - filenames
+# 5 - upload_to_librarian: global boolean trigger
+# 6 - librarian_ant_metrics: boolean trigger for this step
+# 7+ - filenames
 crossCut=${1}
 deadCut=${2}
 Nbls_per_load=${3}
 extension=${4}
-all_ants_statuses=${5}
-upload_to_librarian="${6}"
-librarian_ant_metrics="${7}"
-fn1=`basename ${8}`
-data_files="${@:8}"
+upload_to_librarian="${5}"
+librarian_ant_metrics="${6}"
+fn1=`basename ${7}`
+data_files="${@:7}"
 
 # get exants from HERA CM database
-jd=$(get_jd ${fn1})
-apriori_xants=`query_ex_ants.py ${jd} ${all_ants_statuses}`
 
 # We only want to run ant metrics on sum files
-echo ant_metrics_run.py ${data_files} --crossCut ${crossCut} --deadCut ${deadCut} --extension ${extension} --Nbls_per_load ${Nbls_per_load} \
-    --clobber --apriori_xants ${apriori_xants}
-ant_metrics_run.py ${data_files} --crossCut ${crossCut} --deadCut ${deadCut} --extension ${extension} --Nbls_per_load ${Nbls_per_load} \
-    --clobber --apriori_xants ${apriori_xants}
+echo ant_metrics_run.py ${data_files} --crossCut ${crossCut} --deadCut ${deadCut} --extension ${extension} --Nbls_per_load ${Nbls_per_load} --clobber
+ant_metrics_run.py ${data_files} --crossCut ${crossCut} --deadCut ${deadCut} --extension ${extension} --Nbls_per_load ${Nbls_per_load} --clobber
 
 # upload results to librarian if desired
 if [ "${upload_to_librarian}" == "True" ]; then
