7cd594a66af2114c83e2f88dd1506990f16e38cd
Josh Dillon <jsdillon@berkeley.edu>
2021-02-25 17:11:10 -0800

    update redcal_known_good to take auto_metrics and a priori ex_ants

--- pipelines/h4c/rtp/v1/stage_1_task_scripts/do_REDCAL_KNOWN_GOOD.sh
+++ pipelines/h4c/rtp/v1/stage_1_task_scripts/do_REDCAL_KNOWN_GOOD.sh
@@ -18,10 +18,11 @@ source ${src_dir}/_common.sh
 # 7 - nInt_to_load: number of integrations to load and calibrate simultaneously. Lower numbers save memory, but incur a CPU overhead.
 # 8 - min_bl_cut: cut redundant groups with average baseline lengths shorter than this length in meters
 # 9 - max_bl_cut: cut redundant groups with average baseline lengths longer than this length in meters
-# 10 - ant_metrics_extension: file extension to replace .uvh5 with to get known good ant_metrics files
-# 11 - max_dims: maximum allowed tip/tilt phase degeneracies of redcal. 2 is classically redundant.
-# 12 - upload_to_librarian: global boolean trigger
-# 13 - librarian_redcal_known_good: boolean trigger for this step
+# 10 - max_dims: maximum allowed tip/tilt phase degeneracies of redcal. 2 is classically redundant.
+# 11 - ant_metrics_extension: file extension to replace .uvh5 with to get known good ant_metrics files
+# 12 - good_statuses: string list of comma-separated (no spaces) antenna statuses that represent "good" antennas
+# 13 - upload_to_librarian: global boolean trigger
+# 14 - librarian_redcal_known_good: boolean trigger for this step
 fn="${1}"
 ant_z_thresh="${2}"
 solar_horizon="${3}"
@@ -31,27 +32,46 @@ oc_maxiter="${6}"
 nInt_to_load="${7}"
 min_bl_cut="${8}"
 max_bl_cut="${9}"
-ant_metrics_extension="${10}"
-max_dims="${11}"
-upload_to_librarian="${12}"
-librarian_redcal_known_good="${13}"
+max_dims="${10}"
+ant_metrics_extension="${11}"
+good_statuses="${12}"
+upload_to_librarian="${13}"
+librarian_redcal_known_good="${14}"
 
 # get ant_metrics file, removing extension and appending ant_metrics_extension
-metrics_f=`echo ${fn%.uvh5}${ant_metrics_extension}`
+ant_metrics_file=`echo ${fn%.uvh5}${ant_metrics_extension}`
+
+# get auto_metrics_file
+jd=$(get_int_jd ${fn})
+decimal_jd=$(get_jd ${fn})
+pattern="${fn%${decimal_jd}.sum.uvh5}${jd}.?????.sum.auto_metrics.h5"
+pattern_files=( $pattern )
+auto_metrics_file=${pattern_files[0]}
+
+# get exants from HERA CM database
+ex_ants_db=`query_ex_ants.py ${jd} ${good_statuses}`
 
 # run redcal
-echo redcal_run.py ${fn} --ant_z_thresh ${ant_z_thresh} --solar_horizon ${solar_horizon} --oc_maxiter ${oc_maxiter} \
-    --firstcal_ext .known_good.first.calfits --omnical_ext .known_good.omni.calfits \
-    --omnivis_ext .known_good.omni_vis.uvh5 --meta_ext .known_good.redcal_meta.hdf5 \
-    --flag_nchan_low ${flag_nchan_low} --flag_nchan_high ${flag_nchan_high} --nInt_to_load ${nInt_to_load} \
-    --min_bl_cut ${min_bl_cut} --max_bl_cut ${max_bl_cut} --ant_metrics_file ${metrics_f} \
-    --max_dims ${max_dims} --clobber --verbose
-redcal_run.py ${fn} --ant_z_thresh ${ant_z_thresh} --solar_horizon ${solar_horizon} --oc_maxiter ${oc_maxiter} \
-    --firstcal_ext .known_good.first.calfits --omnical_ext .known_good.omni.calfits \
-    --omnivis_ext .known_good.omni_vis.uvh5 --meta_ext .known_good.redcal_meta.hdf5 \
-    --flag_nchan_low ${flag_nchan_low} --flag_nchan_high ${flag_nchan_high} --nInt_to_load ${nInt_to_load} \
-    --min_bl_cut ${min_bl_cut} --max_bl_cut ${max_bl_cut} --ant_metrics_file ${metrics_f} \
-    --max_dims ${max_dims} --clobber --verbose
+cmd="redcal_run.py ${fn} \
+                   --ant_z_thresh ${ant_z_thresh} \
+                   --solar_horizon ${solar_horizon} \
+                   --oc_maxiter ${oc_maxiter} \
+                   --firstcal_ext .known_good.first.calfits \
+                   --omnical_ext .known_good.omni.calfits \
+                   --omnivis_ext .known_good.omni_vis.uvh5 \
+                   --meta_ext .known_good.redcal_meta.hdf5 \
+                   --flag_nchan_low ${flag_nchan_low} \
+                   --flag_nchan_high ${flag_nchan_high} \
+                   --nInt_to_load ${nInt_to_load} \
+                   --min_bl_cut ${min_bl_cut} \
+                   --max_bl_cut ${max_bl_cut} \
+                   --max_dims ${max_dims} \
+                   --metrics_files ${ant_metrics_file} ${auto_metrics_file} \
+                   --ex_ants ${ex_ants_db} \
+                   --clobber \
+                   --verbose"
+echo $cmd
+$cmd
 
 # add data products to librarian if desired
 if [ "${upload_to_librarian}" == "True" ]; then
