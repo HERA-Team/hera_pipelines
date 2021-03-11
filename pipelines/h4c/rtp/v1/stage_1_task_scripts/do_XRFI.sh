3af93a2bf75a6015501973d3b038c27c1229bb74
Josh Dillon <jsdillon@berkeley.edu>
2021-03-11 13:02:11 -0800

    fix pattern matching

--- pipelines/h4c/rtp/v1/stage_1_task_scripts/do_XRFI.sh
+++ pipelines/h4c/rtp/v1/stage_1_task_scripts/do_XRFI.sh
@@ -21,7 +21,8 @@ data_files="${@:7}"
 
 # get auto_metrics_file to exclude bad antennas
 jd=$(get_int_jd ${data_files[0]})
-decimal_jd=$(get_jd ${data_files[0]})
+bn=`basename ${data_files[0]}`
+decimal_jd=$(get_jd ${bn})
 pattern="${fn%${decimal_jd}.sum.uvh5}${jd}.?????.sum.auto_metrics.h5"
 pattern_files=( $pattern )
 auto_metrics_file=${pattern_files[0]}
