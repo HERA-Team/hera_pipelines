a2bceae45eddd355f655866c596cf7aaa69688d6
Josh Dillon <jsdillon@berkeley.edu>
2021-04-02 09:32:05 -0700

    change master to main

--- pipelines/h4c/rtp/v1/stage_1_task_scripts/do_DATA_INSPECT_NOTEBOOK_KNOWN_GOOD.sh
+++ pipelines/h4c/rtp/v1/stage_1_task_scripts/do_DATA_INSPECT_NOTEBOOK_KNOWN_GOOD.sh
@@ -43,11 +43,11 @@ jupyter nbconvert --output=${nb_outfile} \
 if [ "${git_push}" == "True" ]
 then
     cd ${nb_output_repo}
-    git pull origin master
+    git pull origin main
     git add ${nb_outfile}
     python ${src_dir}/build_notebook_readme.py ${nb_outdir}
     git add ${nb_outdir}/README.md
     lasturl=`python -c "readme = open('${nb_outdir}/README.md', 'r'); print(readme.readlines()[-1].split('(')[-1].split(')')[0])"`
     git commit -m "RTP data inspection of known good antennas for JD ${jd}" -m ${lasturl}
-    git push origin master
+    git push origin main
 fi
