0472b70b7db044a3b73438518e78e87cb2808f80
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-04-09 10:06:16 -0700

    fix pre_chunk auto issue.

--- pipelines/h4c/post_processing/v3/pre_lstbin/task_scripts/do_PRE_CHUNK.sh
+++ pipelines/h4c/post_processing/v3/pre_lstbin/task_scripts/do_PRE_CHUNK.sh
@@ -30,7 +30,7 @@ do
   # iterate over files until we reach the specific jd
   visited_fname="false"
   counter=0
-  for filename in zen.${int_jd}.*.${sd}.autos.uvh5
+  for filename in zen.${int_jd}.*.sum.smooth_abs.calfits
   do
     jd_temp=$(get_jd $filename)
     if [ "${jd_temp}" = "${jd}" ]
@@ -50,13 +50,15 @@ do
       cal_file=zen.${jd_temp}.sum.smooth_abs.calfits
       input_file=${stage_dir}/${int_jd}/zen.${jd_temp}.${sd}.uvh5
       output_file=${stage_dir}/${int_jd}/zen.${jd_temp}.${sd}.uvh5
+
+      # extract the auto file from staged data in case its missing.
+      input_auto=zen.${jd_temp}.${sd}.autos.uvh5
+      extract_autos.py ${input_file} ${input_auto} --clobber
+
       echo apply_cal.py --new_cal ${cal_file} --clobber ${input_file} ${output_file}
       apply_cal.py --new_cal ${cal_file} --clobber  ${input_file} ${output_file}
 
       # calibrate auto file.
-      # extract the auto file from staged data in case its missing.
-      input_auto=zen.${jd_temp}.${sd}.autos.uvh5
-      extract_autos.py ${input_file} ${input_auto} --clobber
       output_auto=zen.${jd_temp}.${sd}.${label}.autos.calibrated.uvh5
       echo apply_cal.py ${input_auto} ${output_auto} --clobber --new_cal ${cal_file}
       apply_cal.py ${input_auto} ${output_auto} --clobber --new_cal ${cal_file}
