9a6d71bbd1120d5e3c37b7a8e0da8d4066b71ed5
Josh Dillon <jsdillon@berkeley.edu>
2021-04-16 15:58:26 -0700

    update to use new red_average script

--- pipelines/h1c/idr3/v2/task_scripts/do_RED_AVG.sh
+++ pipelines/h1c/idr3/v2/task_scripts/do_RED_AVG.sh
@@ -8,13 +8,10 @@ source ${src_dir}/_common.sh
 # Parameters are set in the configuration file, here we define their positions,
 # which must be consistent with the config.
 # 1 - filename
-# 2 - baselines to load at once.
-
 fn="${1}"
-nbl_per_load="${2}"
 
 # make outfile
 outfile=${fn%.uvh5}.red_avg.uvh5
 
-echo apply_cal.py ${fn} ${outfile} --nbl_per_load ${nbl_per_load} --redundant_average --clobber
-apply_cal.py ${fn} ${outfile} --nbl_per_load ${nbl_per_load} --redundant_average --clobber
+echo red_average.py ${fn} ${outfile} --redundant_average --clobber
+red_average.py ${fn} ${outfile} --redundant_average --clobber
