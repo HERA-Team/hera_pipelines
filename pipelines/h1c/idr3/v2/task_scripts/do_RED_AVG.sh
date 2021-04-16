ab74b7f52c44748ee7377b9dfdf26013741c2505
Josh Dillon <jsdillon@berkeley.edu>
2021-04-16 12:32:14 -0700

    Create do_RED_AVG.sh

---
+++ pipelines/h1c/idr3/v2/task_scripts/do_RED_AVG.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# import common funcitons
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# Parameters are set in the configuration file, here we define their positions,
+# which must be consistent with the config.
+# 1 - filename
+# 2 - baselines to load at once.
+
+fn="${1}"
+nbl_per_load="${2}"
+
+# make outfile
+outfile=${fn%.uvh5}.red_avg.uvh5
+
+echo apply_cal.py ${uvh5_fn} ${outfile} --nbl_per_load ${nbl_per_load} --redundant_average --clobber
+apply_cal.py ${uvh5_fn} ${outfile} --nbl_per_load ${nbl_per_load} --redundant_average --clobber
