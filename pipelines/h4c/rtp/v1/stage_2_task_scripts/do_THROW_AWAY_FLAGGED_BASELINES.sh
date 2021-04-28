f81350ff7923e3be6831349a5e2ad988f6628601
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-04-28 13:10:21 -0700

    stage_2_toml that throws away flagged antennas.

---
+++ pipelines/h4c/rtp/v1/stage_2_task_scripts/do_THROW_AWAY_FLAGGED_BASELINES.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+set -e
+
+# This script throws away baselines from antennas that are flagged in apriori yaml file.
+
+# import common functions
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+
+fn=${1}
+yaml_dir=${2}
+
+jd=$(get_jd `${fn}`)
+jd_int=$(get_int_jd `${fn}`)
+yaml_file=${yaml_dir}/${jd_int}.yaml
+
+sumdiff=("sum" "diff")
+for sd in ${sumdiff[@]}
+do
+  input=zen.${jd}.${sd}.uvh5
+  output=zen.${jd}.${sd}.uvh5
+  output_autos=zen.${jd}.${sd}.autos.uvh5
+  echo throw_away_flagged_antennas.py ${input} ${output} --yaml_file ${yaml_file} --clobber
+  throw_away_flagged_antennas.py ${input} ${output} --yaml_file ${yaml_file} --clobber
+  # extract autos
+  echo extract_autos.py ${output} ${output_autos} --clobber
+done
