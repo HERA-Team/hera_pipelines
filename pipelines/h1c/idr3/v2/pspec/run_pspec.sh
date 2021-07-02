7d8ccf89c702c168dd16ca1314fd6bb491909bff
Josh Dillon <jsdillon@berkeley.edu>
2021-07-01 17:00:36 -0700

    update shell script for h1c_idr3 pspec

--- pipelines/h1c/idr3/v2/pspec/run_pspec.sh
+++ pipelines/h1c/idr3/v2/pspec/run_pspec.sh
@@ -3,16 +3,17 @@
 #PBS -j oe
 #PBS -o pspec_pipe.out
 #PBS -N pspec_pipe
-#PBS -l nodes=1:ppn=8
+#PBS -l nodes=1:ppn=15
 #PBS -l walltime=96:00:00
-#PBS -l vmem=122GB,mem=122GB
-#PBS -M nkern@berkeley.edu
+#PBS -l vmem=250GB,mem=250GB
+#PBS -M jsdillon+nrao@berkeley.edu
 
 source ~/.bashrc
-conda activate hera3
-cd /lustre/aoc/projects/hera/H1C_IDR2/IDR2_2_pspec/v2/one_group
+conda activate h1c_idr3
+cd /lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/LSTBIN/all_epochs/pspec
 
 echo "start: $(date)"
-/users/heramgr/hera_software/H1C_IDR2/pipeline/pspec_pipe.py pspec_params.yaml
+preprocess_dir=/lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/h1c_idr3_software/hera_pipelines/pipelines/h1c/idr3/v2/pspec/
+${preprocess_dir}/pspec_pipe.py ${preprocess_dir}/pspec_params.yaml 
 
 echo "end: $(date)"
