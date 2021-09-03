7cc29cf0dd2cd5f7a969d9785f4a05dec3f86f53
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-09-03 08:22:06 -0700

    change to conda_sh

--- pipelines/calamity/stage_data/ilifu_makeflow/run_build_makeflow.sh
+++ pipelines/calamity/stage_data/ilifu_makeflow/run_build_makeflow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source $CONDA_INIT_SCRIPT
+source $CONDA_SH
 conda activate calamity
 cd /ilifu/astro/projects/hera/aewallwi/calamity/makeflow/
 build_makeflow_from_config.py -c /ilifu/astro/projects/hera/aewallwi/hera_pipelines/pipelines/calamity/stage_data/stage_data.toml /ilifu/astro/projects/hera/aewallwi/calamity/mwa/*.metafits
