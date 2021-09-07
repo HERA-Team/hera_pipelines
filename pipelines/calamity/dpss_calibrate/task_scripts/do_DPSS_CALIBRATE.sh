341a51c95b66be8dad9fe4f2ca1083245d779792
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-09-07 13:05:33 -0700

    add bllen selections.

--- pipelines/calamity/dpss_calibrate/task_scripts/do_DPSS_CALIBRATE.sh
+++ pipelines/calamity/dpss_calibrate/task_scripts/do_DPSS_CALIBRATE.sh
@@ -7,6 +7,9 @@ fn="${1}"
 horizon="${2}"
 offset="${3}"
 min_dly="${4}"
+bllen_min="${5}"
+bllen_max="${6}"
+bl_ew_min="${7}"
 
 jd=$(get_jd $fn)
 int_jd=${jd:0:7}
@@ -22,8 +25,8 @@ fn_gain=zen.${jd}.gain_fit.calfits
 echo calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename\
  ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain}\
   --verbose --graph_mode --red_tol 0.3 --horizon ${horizon} --offset ${offset}\
-  --min_dly ${min_dly}
+  --min_dly ${min_dly} --bllen_max ${bllen_max} --bllen_min ${bllen_min} --bl_ew_min ${bl_ew_min}
 calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename\
  ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain}\
   --verbose --graph_mode --red_tol 0.3 --horizon ${horizon} --offset ${offset}\
-  --min_dly ${min_dly}
+  --min_dly ${min_dly} --bllen_max ${bllen_max} --bllen_min ${bllen_min} --bl_ew_min ${bl_ew_min}
