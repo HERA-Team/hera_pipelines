3f6c932991034e360c7f59ededcd5aaf35f44565
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-09-07 00:45:05 -0700

    use lower red_tol

--- pipelines/calamity/dpss_calibrate/task_scripts/do_DPSS_CALIBRATE.sh
+++ pipelines/calamity/dpss_calibrate/task_scripts/do_DPSS_CALIBRATE.sh
@@ -18,5 +18,5 @@ fn_resid=zen${jd}.resid_fit.uvh5
 fn_model=zen${jd}.model_fit.uvh5
 fn_gain=zen${jd}.gain_fit.calfits
 
-echo calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain} --verbose --graph_mode
-calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain} --verbose --graph_mode
+echo calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain} --verbose --graph_mode --red_tol 0.3
+calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain} --verbose --graph_mode --red_tol 0.3
