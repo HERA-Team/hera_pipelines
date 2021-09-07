5ca27616816b801dbe56af7daa928afd9a66c7e1
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-09-07 00:43:58 -0700

    use graph mode.

--- pipelines/calamity/dpss_calibrate/task_scripts/do_DPSS_CALIBRATE.sh
+++ pipelines/calamity/dpss_calibrate/task_scripts/do_DPSS_CALIBRATE.sh
@@ -18,5 +18,5 @@ fn_resid=zen${jd}.resid_fit.uvh5
 fn_model=zen${jd}.model_fit.uvh5
 fn_gain=zen${jd}.gain_fit.calfits
 
-echo calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain} --verbose
-calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain} --verbose
+echo calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain} --verbose --graph_mode
+calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain} --verbose --graph_mode
