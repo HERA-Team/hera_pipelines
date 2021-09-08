f50692ccc404bb0c9116a363b063066d9c3c1f7e
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-09-08 00:07:16 -0700

    use hera3gpu env.

--- pipelines/calamity/dpss_calibrate/task_scripts/do_DPSS_CALIBRATE.sh
+++ pipelines/calamity/dpss_calibrate/task_scripts/do_DPSS_CALIBRATE.sh
@@ -3,13 +3,16 @@ set -e
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
 
-fn="${1}"
-horizon="${2}"
-offset="${3}"
-min_dly="${4}"
-bllen_min="${5}"
-bllen_max="${6}"
-bl_ew_min="${7}"
+horizon="${1}"
+offset="${2}"
+min_dly="${3}"
+bllen_min="${4}"
+bllen_max="${5}"
+bl_ew_min="${6}"
+ex_ants="${7}"
+data_files="${@:8}"
+
+ex_ants="${ex_ants//,/$' '}"
 
 jd=$(get_jd $fn)
 int_jd=${jd:0:7}
@@ -22,11 +25,13 @@ fn_resid=zen.${jd}.resid_fit.uvh5
 fn_model=zen.${jd}.model_fit.uvh5
 fn_gain=zen.${jd}.gain_fit.calfits
 
-echo calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename\
+echo calibrate_and_model_dpss.py --input_data_files ${data_files} --model_outfilename\
  ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain}\
-  --verbose --graph_mode --red_tol 0.3 --horizon ${horizon} --offset ${offset}\
-  --min_dly ${min_dly} --bllen_max ${bllen_max} --bllen_min ${bllen_min} --bl_ew_min ${bl_ew_min}
-calibrate_and_model_dpss.py --input_data_files ${fn} --model_outfilename\
+  --verbose --red_tol 0.3 --horizon ${horizon} --offset ${offset}\
+  --min_dly ${min_dly} --bllen_max ${bllen_max} --bllen_min ${bllen_min} --bl_ew_min ${bl_ew_min}\
+  --ex_ants ${ex_ants}
+calibrate_and_model_dpss.py --input_data_files ${data_files} --model_outfilename\
  ${fn_model} --resid_outfilename ${fn_resid} --gain_outfilename ${fn_gain}\
-  --verbose --graph_mode --red_tol 0.3 --horizon ${horizon} --offset ${offset}\
-  --min_dly ${min_dly} --bllen_max ${bllen_max} --bllen_min ${bllen_min} --bl_ew_min ${bl_ew_min}
+  --verbose --red_tol 0.3 --horizon ${horizon} --offset ${offset}\
+  --min_dly ${min_dly} --bllen_max ${bllen_max} --bllen_min ${bllen_min} --bl_ew_min ${bl_ew_min}\
+  --ex_ants ${ex_ants}
