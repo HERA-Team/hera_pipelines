fdf0568a2009e520f0dcdde064e7d74c51282f9e
Josh Dillon <jsdillon@berkeley.edu>
2021-09-24 09:21:45 -0700

    fix PXTK per epoch pspec

--- pipelines/h1c/idr3/v2/pspec/run_pspec_epoch_0_PXTK.sh
+++ pipelines/h1c/idr3/v2/pspec/run_pspec_epoch_0_PXTK.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -q hera
 #PBS -j oe
-#PBS -o pspec_pipe_PTXK_e0.out
-#PBS -N pspec_pipe_PTXK_e0
+#PBS -o pspec_pipe_PXTK_e0.out
+#PBS -N pspec_pipe_PXTK_e0
 #PBS -l nodes=1:ppn=15
 #PBS -l walltime=96:00:00
 #PBS -l vmem=250GB,mem=250GB
@@ -14,6 +14,6 @@ cd /lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/LSTBIN/epoch_0/pspec
 
 echo "start: $(date)"
 pspec_dir=/lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/h1c_idr3_software/hera_pipelines/pipelines/h1c/idr3/v2/pspec/
-${pspec_dir}/pspec_pipe.py ${pspec_dir}/pspec_params_epoch_0_PTXK.yaml 
+${pspec_dir}/pspec_pipe.py ${pspec_dir}/pspec_params_epoch_0_PXTK.yaml 
 
 echo "end: $(date)"
--- pipelines/h1c/idr3/v2/pspec/run_pspec_epoch_3_PXTK.sh
+++ pipelines/h1c/idr3/v2/pspec/run_pspec_epoch_3_PXTK.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -q hera
 #PBS -j oe
-#PBS -o pspec_pipe_PTXK_e3.out
-#PBS -N pspec_pipe_PTXK_e3
+#PBS -o pspec_pipe_PXTK_e3.out
+#PBS -N pspec_pipe_PXTK_e3
 #PBS -l nodes=1:ppn=15
 #PBS -l walltime=96:00:00
 #PBS -l vmem=250GB,mem=250GB
@@ -14,6 +14,6 @@ cd /lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/LSTBIN/epoch_3/pspec
 
 echo "start: $(date)"
 pspec_dir=/lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/h1c_idr3_software/hera_pipelines/pipelines/h1c/idr3/v2/pspec/
-${pspec_dir}/pspec_pipe.py ${pspec_dir}/pspec_params_epoch_3_PTXK.yaml 
+${pspec_dir}/pspec_pipe.py ${pspec_dir}/pspec_params_epoch_3_PXTK.yaml 
 
 echo "end: $(date)"
