72ade13b1f6486f2b58d8dd54e406763cb354ee0
Josh Dillon <jsdillon@berkeley.edu>
2021-09-30 09:05:05 -0700

    change to run second round tavg and pstokes on new LST-binned data

--- pipelines/h1c/idr3/v2/preprocess/run_preproc_LPXLTK.sh
+++ pipelines/h1c/idr3/v2/preprocess/run_preproc_LPXLTK.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -q hera
 #PBS -j oe
-#PBS -o preproc_pipe.out
-#PBS -N preproc_pipe
+#PBS -o preproc_LPXLTK.out
+#PBS -N preproc_LPXLTK
 #PBS -l nodes=1:ppn=15
 #PBS -l walltime=256:00:00
 #PBS -l vmem=250GB,mem=250GB
@@ -14,6 +14,6 @@ cd /lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/LSTBIN/all_epochs/preprocess
 
 echo "start: $(date)"
 preprocess_dir=/lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/h1c_idr3_software/hera_pipelines/pipelines/h1c/idr3/v2/preprocess
-${preprocess_dir}/preprocess_data.py ${preprocess_dir}/reprocess_params_LPXLPTK.yaml
+${preprocess_dir}/preprocess_data.py ${preprocess_dir}/reprocess_params_LPXLTK.yaml
 
 echo "end: $(date)"
