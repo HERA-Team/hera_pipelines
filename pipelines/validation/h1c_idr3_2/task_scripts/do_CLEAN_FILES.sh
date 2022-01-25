e2a8af350e4e19c0847e32eb9b7c313e58d54782
Josh Dillon <jsdillon@berkeley.edu>
2022-01-24 17:51:31 -0800

    Comment out removes that we don't need for validation

--- pipelines/validation/h1c_idr3_2/task_scripts/do_CLEAN_FILES.sh
+++ pipelines/validation/h1c_idr3_2/task_scripts/do_CLEAN_FILES.sh
@@ -9,24 +9,25 @@ source ${src_dir}/_common.sh
 fn="${1}"
 
 # remove miriad files
-fn_xx=${fn}
-fn_yy=$(replace_pol $fn "yy")
-fn_xy=$(replace_pol $fn "xy")
-fn_yx=$(replace_pol $fn "yx")
-echo rm -rf "${fn_xx}"
-rm -rf "${fn_xx}"
-echo rm -rf "${fn_yy}"
-rm -rf "${fn_yy}"
-echo rm -rf "${fn_xy}"
-rm -rf "${fn_xy}"
-echo rm -rf "${fn_yx}"
-rm -rf "${fn_yx}"
+# fn_xx=${fn}
+# fn_yy=$(replace_pol $fn "yy")
+# fn_xy=$(replace_pol $fn "xy")
+# fn_yx=$(replace_pol $fn "yx")
+# echo rm -rf "${fn_xx}"
+# rm -rf "${fn_xx}"
+# echo rm -rf "${fn_yy}"
+# rm -rf "${fn_yy}"
+# echo rm -rf "${fn_xy}"
+# rm -rf "${fn_xy}"
+# echo rm -rf "${fn_yx}"
+# rm -rf "${fn_yx}"
 
 # remove uvh5 files
-uvh5_fn=$(remove_pol ${fn})
-uvh5_fn=${uvh5_fn%.HH.uv}.sum.uvh5 # this makes things more compatible with H3C/H4C software
-echo rm -rfv ${uvh5_fn}
-rm -rfv ${uvh5_fn}
+# TODO: put this back in
+# uvh5_fn=$(remove_pol ${fn})
+# uvh5_fn=${uvh5_fn%.HH.uv}.sum.uvh5 # this makes things more compatible with H3C/H4C software
+# echo rm -rfv ${uvh5_fn}
+# rm -rfv ${uvh5_fn}
 
 # remove firstcal files
 firstcal_fn=${uvh5_fn%.uvh5}.first.calfits
