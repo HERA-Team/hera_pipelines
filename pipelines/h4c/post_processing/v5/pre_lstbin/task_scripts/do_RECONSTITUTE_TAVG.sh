b6a9c478c923bbd6f1372aff20248e5a1a380194
Aaron Ewall-Wice <aaronew@berkeley.edu>
2022-02-06 11:41:46 -0800

    update reconst tavg.

--- pipelines/h4c/post_processing/v5/pre_lstbin/task_scripts/do_RECONSTITUTE_TAVG.sh
+++ pipelines/h4c/post_processing/v5/pre_lstbin/task_scripts/do_RECONSTITUTE_TAVG.sh
@@ -43,16 +43,20 @@ do
       time_chunk_from_baseline_chunks_run.py ${time_chunk_template} --outfilename ${outfilename}\
           --baseline_chunk_files ${baseline_chunk_files} --clobber --time_bounds
     done
+      nchunks=`ls zen.${int_jd}.*.${sd}.${label}.foreground_filled.time_inpainted.waterfall.uvh5 | wc -l`
+      # check that time inpainted files exist.
+      if [ "${nchunks}" != "0" ]
+      then
+        # reconstitute fr-inpainted time-averaged files
+        outfilename=zen.${jd}.${sd}.${label}.foreground_filled.time_inpainted.tavg.uvh5
+        baseline_chunk_files=`echo zen.${int_jd}.*.${sd}.${label}.foreground_filled.time_inpainted.waterfall.tavg.uvh5`
 
-      # reconstitute fr-inpainted time-averaged files
-      outfilename=zen.${jd}.${sd}.${label}.foreground_filled.time_inpainted.tavg.uvh5
-      baseline_chunk_files=`echo zen.${int_jd}.*.${sd}.${label}.foreground_filled.time_inpainted.waterfall.tavg.uvh5`
+        echo time_chunk_from_baseline_chunks_run.py ${time_chunk_template} --outfilename ${outfilename}\
+            --baseline_chunk_files ${baseline_chunk_files} --clobber --time_bounds
 
-      echo time_chunk_from_baseline_chunks_run.py ${time_chunk_template} --outfilename ${outfilename}\
-          --baseline_chunk_files ${baseline_chunk_files} --clobber --time_bounds
-
-      time_chunk_from_baseline_chunks_run.py ${time_chunk_template} --outfilename ${outfilename}\
-          --baseline_chunk_files ${baseline_chunk_files} --clobber --time_bounds
+        time_chunk_from_baseline_chunks_run.py ${time_chunk_template} --outfilename ${outfilename}\
+            --baseline_chunk_files ${baseline_chunk_files} --clobber --time_bounds
+      fi
   else
     echo "${time_chunk_template} does not exist!"
   fi
