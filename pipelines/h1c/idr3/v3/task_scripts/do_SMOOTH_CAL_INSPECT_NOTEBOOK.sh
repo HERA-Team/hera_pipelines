7da221423ffa09c27b864b87709ee4585a3b50f7
Josh Dillon <jsdillon@berkeley.edu>
2022-02-08 17:31:56 -0800

    add back rest of core pipeline scripts

---
+++ pipelines/h1c/idr3/v3/task_scripts/do_SMOOTH_CAL_INSPECT_NOTEBOOK.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+set -e
+
+# This script generates a notebook for inspecting the results of callibration smoothing
+
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# Parameters are set in the configuration file. Here we define their positions,
+# which must be consistent with the config.
+# 1 - (raw) filename
+# 2 - nb_template_dir: where to look for the notebook template
+# 3 - nb_output_repo: repository for saving evaluated notebooks
+# 4 - git_push: boolean whether to push the results created in the nb_output_repo
+# 5 - glob-parsable string pointing to model_files
+# 6+ - lst_blacklists for smooth cal
+fn=${1}
+nb_template_dir=${2}
+nb_output_repo=${3}
+git_push=${4}
+model_files_glob=${5}
+lst_blacklists="${@:6}"
+
+# Get JD from filename
+jd=$(get_int_jd ${fn})
+nb_outfile=${nb_output_repo}/smooth_cal_inspect/smooth_cal_inspect_${jd}.ipynb
+
+# Export variables used by the notebook
+export DATA_PATH=`pwd`
+export JULIANDATE=${jd}
+export ABSCAL_MODEL_GLOB=${model_files_glob}
+export LST_BLACKLIST_STRING=${lst_blacklists}
+
+# Execute jupyter notebook
+jupyter nbconvert --output=${nb_outfile} \
+--to notebook \
+--ExecutePreprocessor.allow_errors=True \
+--ExecutePreprocessor.timeout=-1 \
+--execute ${nb_template_dir}/stage_2_smooth_cal.ipynb
+
+# If desired, push results to github
+if [ "${git_push}" == "True" ]
+then
+    cd ${nb_output_repo}
+    git pull origin master
+    git add ${nb_outfile}
+    git commit -m "H1C IDR3 smooth_cal notebook for JD ${jd}"
+    git push origin master
+fi
