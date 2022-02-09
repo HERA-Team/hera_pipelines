75a8ad498c84b6ac3f0ded9ba1ac2f6fa1a6ebbc
Josh Dillon <jsdillon@berkeley.edu>
2022-02-08 16:49:02 -0800

    update uvh5 creation to also throw out flagged antennas

---
+++ pipelines/h1c/idr3/v3/task_scripts/do_MAKE_DOWNSELCTED_UVH5.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# import common functions
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# define input arguments
+fn="${1}"
+path_to_a_priori_flags="${2}"
+
+# combine all 4 polarizations into a single file using pyuvdata
+fn_xx=${fn}
+fn_yy=$(replace_pol $fn "yy")
+fn_xy=$(replace_pol $fn "xy")
+fn_yx=$(replace_pol $fn "yx")
+fn_out=$(remove_pol $fn)
+fn_out=${fn_out%.HH.uv}.sum.uvh5 # this makes things more compatible with H3C/H4C software
+
+# merge four polarizations
+echo python -c "from pyuvdata import UVData; \
+                uv = UVData(); \
+                uv.read([\"${fn_xx}\", \"${fn_yy}\", \"${fn_xy}\", \"${fn_yx}\"], \
+                axis=\"polarization\"); \
+                uv.x_orientation = 'east'; \
+                uv.history += '\n\nx_orientation manually set to east\n\n'; \
+                uv.write_uvh5(\"${fn_out}\", clobber=True)"
+python -c "from pyuvdata import UVData; \
+           uv = UVData(); \
+           uv.read([\"${fn_xx}\", \"${fn_yy}\", \"${fn_xy}\", \"${fn_yx}\"], \
+           axis=\"polarization\"); \
+           uv.x_orientation = 'east'; \
+           uv.history += '\n\nx_orientation manually set to east\n\n'; \
+           uv.write_uvh5(\"${fn_out}\", clobber=True)"
+
+# throw out flagged antennas
+ex_ants_yaml=`echo "${path_to_a_priori_flags}/${jd_int}.yaml"`
+echo throw_away_flagged_antennas.py ${fn_out} ${fn_out} --yaml_file ${ex_ants_yaml} --clobber
+throw_away_flagged_antennas.py ${fn_out} ${fn_out} --yaml_file ${ex_ants_yaml} --clobber
