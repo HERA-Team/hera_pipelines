b28d86a708c09aced79e325f85706d6c7075ca45
Josh Dillon <jsdillon@berkeley.edu>
2022-02-18 09:41:49 -0800

    update pipeline in response to https://github.com/HERA-Team/hera_qm/pull/411

--- pipelines/h5c/rtp/v1/stage_1_task_scripts/do_XRFI.sh
+++ pipelines/h5c/rtp/v1/stage_1_task_scripts/do_XRFI.sh
@@ -12,12 +12,14 @@ source ${src_dir}/_common.sh
 ### XRFI parameters - see hera_qm.utils for details
 # 1 - kt_size
 # 2 - kf_size
-# 3 - sig_init
-# 4 - sig_adj
-# 5 - Nwf_per_load
-# 6 - ant_metrics_ext
-# 7+ - filenames
-data_files=(${@:7})
+# 3 - sig_init_med
+# 4 - sig_adj_med
+# 5 - sig_init_mean
+# 6 - sig_adj_mean
+# 7 - Nwf_per_load
+# 8 - ant_metrics_ext
+# 9+ - filenames
+data_files=(${@:9})
 
 # get auto_metrics_file to exclude bad antennas
 fn=${data_files[0]}
@@ -31,7 +33,7 @@ auto_metrics_file=${pattern_files[0]}
 # get ant_metrics_files to exclude bad antennas
 ant_metrics_files=()
 for fn in ${data_files[@]}; do
-    ant_metrics_files+=( ${fn%.uvh5}${6} )
+    ant_metrics_files+=( ${fn%.uvh5}${8} )
 done
 
 # construct autos files from data files 
@@ -44,9 +46,11 @@ done
 cmd="xrfi_run_data_only.py --data_files ${autos_files[@]} \
                            --kt_size=${1} \
                            --kf_size=${2} \
-                           --sig_init=${3} \
-                           --sig_adj=${4} \
-                           --Nwf_per_load=${5} \
+                           --sig_init_med=${3} \
+                           --sig_adj_med=${4} \
+                           --sig_init_mean=${5} \
+                           --sig_adj_mean=${6} \
+                           --Nwf_per_load=${7} \
                            --skip_cross_mean_filter \
                            --metrics_files ${auto_metrics_file} ${ant_metrics_files[@]} \
                            --skip_cross_pol_vis \
