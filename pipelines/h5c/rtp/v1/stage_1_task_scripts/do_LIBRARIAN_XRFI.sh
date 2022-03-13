107123226cb56d298c60e11213a6da4abb04bceb
Zhilei Xu <astro.zlxu@gmail.com>
2022-03-13 11:33:24 -0400

    Modification to compress both the xrfi and the threshold files.

--- pipelines/h5c/rtp/v1/stage_1_task_scripts/do_LIBRARIAN_XRFI.sh
+++ pipelines/h5c/rtp/v1/stage_1_task_scripts/do_LIBRARIAN_XRFI.sh
@@ -18,21 +18,30 @@ if [ "${upload_to_librarian}" == "True" ]; then
     if [ "${librarian_xrfi}" == "True" ]; then
         # get the integer portion of the JD
         jd=$(get_int_jd ${fn})
+        decimal_jd=$(get_jd ${fn})
 
         # get xrfi folder
         xrfi_folder=`echo ${fn%.sum.uvh5}.xrfi`
         xrfi_stage_1_folder=`echo ${fn%.sum.uvh5}.stage_1_xrfi`
         compressed_file=`echo ${fn%.sum.uvh5}.stage_1_xrfi.tar.gz`
         if [ -d "${xrfi_folder}" ]; then
-            echo tar -czfv ${compressed_file} ${xrfi_folder}
-            tar -czfv ${compressed_file} ${xrfi_folder}
-            echo librarian upload local-rtp ${compressed_file} ${jd}/${xrfi_folder%.xrfi}.stage_1_xrfi.tar.gz
-            librarian upload local-rtp ${compressed_file} ${jd}/${xrfi_folder%.xrfi}.stage_1_xrfi.tar.gz
+            echo tar -czfv ${compressed_file} zen.${jd}.*.xrfi
+            tar -czfv ${compressed_file} zen.${jd}.*.xrfi
+            echo librarian upload local-rtp ${compressed_file} zen.${jd}.stage_1_xrfi.tar.gz
+            librarian upload local-rtp ${compressed_file} zen.${jd}.stage_1_xrfi.tar.gz
         elif [ -d "${xrfi_stage_1_folder}" ]; then # if it has already been renamed
-            echo tar -czfv ${compressed_file} ${xrfi_stage_1_folder}
-            tar -czfv ${compressed_file} ${xrfi_stage_1_folder}
-            echo librarian upload local-rtp ${compressed_file} ${jd}/${xrfi_stage_1_folder}.tar.gz
-            librarian upload local-rtp ${compressed_file} ${jd}/${xrfi_stage_1_folder}.tar.gz
+            echo tar -czfv ${compressed_file} zen.${jd}.*.stage_1_xrfi
+            tar -czfv ${compressed_file} zen.${jd}.*.stage_1_xrfi
+            echo librarian upload local-rtp ${compressed_file} zen.${jd}.stage_1_xrfi.tar.gz
+            librarian upload local-rtp ${compressed_file} zen.${jd}.stage_1_xrfi.tar.gz
+            
+        # upload all thresholded flags files        
+        compressed_threshold_file=`echo ${fn%.sum.uvh5}.stage_1_threshold_flags.tar.gz`
+        echo tar -czfv ${compressed_threshold_file} zen.${jd}*_stage_1_threshold_flags.h5
+        tar -czfv ${compressed_threshold_file} zen.${jd}*_stage_1_threshold_flags.h5
+        echo librarian upload local-rtp ${compressed_threshold_file} zen.${jd}.stage_1_threshold_flags.tar.gz
+        librarian upload local-rtp ${compressed_threshold_file} zen.${jd}.stage_1_threshold_flags.tar.gz
+            
         fi
     fi
 fi
