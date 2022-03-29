d65d7835bca16816ba533bab457117e7cd2f4793
Josh Dillon <jsdillon@berkeley.edu>
2022-03-28 19:12:39 -0700

    bring over pspec scripts, tomls, etc. and update for validation and slurm

---
+++ pipelines/validation/h1c_idr3_2/pspec/run_pspec_LPXLTK.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=LPXLTK_pspec
+#SBATCH --partition=hera
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=15
+#SBATCH --mem=250G
+#SBATCH --time=256:00:00
+#SBATCH --output=LPXLTK_pspec.out
+#SBATCH --export=ALL
+
+source ~/.bashrc
+conda activate h1c_idr3_2_validation
+cd /lustre/aoc/projects/hera/Validation/test-4.1.0/LSTBIN/all_epochs_preprocessed/pspec
+
+echo "start: $(date)"
+pspec_dir=/lustre/aoc/projects/hera/Validation/test-4.1.0/software/hera_pipelines/pipelines/validation/h1c_idr3_2/pspec/
+${pspec_dir}/pspec_pipe.py ${pspec_dir}/pspec_params_LPXLTK.yaml
+
+echo "end: $(date)"
