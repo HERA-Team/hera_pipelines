9f61215b74cf2a94c217413cfa0d032d5d266bf4
Josh Dillon <jsdillon@berkeley.edu>
2022-03-28 18:58:56 -0700

    update paths for preprocessing shell scripts

--- pipelines/validation/h1c_idr3_2/preprocess/run_preproc_epoch_0.sh
+++ pipelines/validation/h1c_idr3_2/preprocess/run_preproc_epoch_0.sh
@@ -13,7 +13,7 @@ conda activate h1c_idr3
 cd /lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/LSTBIN/epoch_0/preprocess
 
 echo "start: $(date)"
-preprocess_dir=/lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/h1c_idr3_software/hera_pipelines/pipelines/h1c/idr3/v2/preprocess
+preprocess_dir=/lustre/aoc/projects/hera/H1C_IDR3/IDR3_2/software/hera_pipelines/pipelines/validation/h1c_idr3_2/preprocess
 ${preprocess_dir}/preprocess_data.py ${preprocess_dir}/preprocess_params_epoch_0.yaml 
 
 echo "end: $(date)"
