a1221817e6365471587b81ee1d88585257ef5c6f
Josh Dillon <jsdillon@berkeley.edu>
2022-04-13 10:52:45 -0400

    check match in lsts instead

--- pipelines/validation/h1c_idr3_2/preprocess/truncate_final_file.py
+++ pipelines/validation/h1c_idr3_2/preprocess/truncate_final_file.py
@@ -15,26 +15,26 @@ hd = io.HERAData(args.file_to_truncate)
 d, f, n = hd.read()
 
 # Check if times match and therefore the flag file can be applied
-assert np.all(np.isclose(hd.times[0:len(uvf.time_array)], uvf.time_array,
-                         rtol=max(hd._time_array.tols[0], uvf._time_array.tols[0]),
-                         atol=max(hd._time_array.tols[1], uvf._time_array.tols[1])))
+assert np.all(np.isclose(hd.lsts[0:len(uvf.lst_array)], uvf.lst_array,
+                         rtol=max(hd._lst_array.tols[0], uvf._lst_array.tols[0]),
+                         atol=max(hd._lst_array.tols[1], uvf._lst_array.tols[1])))
 
 # Check if we need to truncate
-if len(uvf.time_array) < len(hd.times):
+if len(uvf.lst_array) < len(hd.lsts):
     # Check that all truncated times are flagged, so we're not losing any data
     for bl in f:
-        assert np.all(f[bl][len(uvf.time_array):, :])
+        assert np.all(f[bl][len(uvf.lst_array):, :])
 
     # Truncate, update history, and write file to disk
     hd.history += '\n\nManually truncated to match H1C IDR 3.2\n\n'
-    hd.select(times=hd.times[0:len(uvf.time_array)])
+    hd.select(times=hd.times[0:len(uvf.lst_array)])
     hd.write_uvh5(args.ffile_to_truncate, clobber=True)
 
     # Also truncate STD file
     hd = io.HERAData(args.file_to_truncate.replace('.LST.','.STD.'))
     d, f, n = hd.read()
     hd.history += '\n\nManually truncated to match H1C IDR 3.2\n\n'
-    hd.select(times=hd.times[0:len(uvf.time_array)])
+    hd.select(times=hd.times[0:len(uvf.lst_array)])
     hd.write_uvh5(args.file_to_truncate.replace('.LST.','.STD.'), clobber=True)
 
 else:
