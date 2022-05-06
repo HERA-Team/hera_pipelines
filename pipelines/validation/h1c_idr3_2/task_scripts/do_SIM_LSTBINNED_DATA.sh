02c2109fef4c28a276e9d38535b8f3ea64684b7a
Josh Dillon <jsdillon@berkeley.edu>
2022-05-05 17:57:09 -0700

    modify to handle inflation as a parameter

--- pipelines/validation/h1c_idr3_2/task_scripts/do_SIM_LSTBINNED_DATA.sh
+++ pipelines/validation/h1c_idr3_2/task_scripts/do_SIM_LSTBINNED_DATA.sh
@@ -13,19 +13,20 @@ source ${src_dir}/_common.sh
 # 2 - sky component (e.g. eor)
 # 3 - path to simulation data files
 # 4 - path to configuration file (use "None" for no systematics or uncalibration)
+# 5 - whether or not to inflate the output by redundancy
 
 ref_file="${1}"
 sky_cmp="${2}"
 sim_dir="${3}"
 config_path="${4}"
+inflate="${5}"
 
 # Assumes this is built off soft links in the output folder to files like zen.grp1.of1.LST.0.02584.sum.LPL.uvh5
 if [ "${config_path}" != "None" ]; then
-    outfile="${ref_file%.sum*}".${sky_cmp}.with_systematics.uvh5
+    outfile="${ref_file%.sum*}".true_${sky_cmp}.with_xtalk.uvh5
 else
-    outfile="${ref_file%.sum*}".${sky_cmp}.uvh5
+    outfile="${ref_file%.sum*}".true_${sky_cmp}.uvh5
 fi
-
 
 # Do the interpolation and systematics simulation.
 cmd="python ${src_dir}/mock_lstbinned_data.py ${ref_file} \
@@ -33,10 +34,11 @@ cmd="python ${src_dir}/mock_lstbinned_data.py ${ref_file} \
                                               ${outfile} \
                                               --sim_dir ${sim_dir} \
                                               --clobber \
-                                              --inflate \
                                               --input_is_compressed"
 if [ "${config_path}" != "None" ]; then
     cmd="${cmd} --config ${config_path}"
 fi
+if [ "${inflate}" != "True" ]; then
+    cmd="${cmd} --inflate"
 echo $cmd
 $cmd
