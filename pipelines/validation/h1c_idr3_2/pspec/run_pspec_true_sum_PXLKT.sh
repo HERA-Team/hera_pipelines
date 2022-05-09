ef52f69386bb9f346dc9682d6fd591fe4260bc9c
Josh Dillon <jsdillon@berkeley.edu>
2022-05-09 15:37:46 -0700

    fix reference to yaml

--- pipelines/validation/h1c_idr3_2/pspec/run_pspec_true_sum_PXLKT.sh
+++ pipelines/validation/h1c_idr3_2/pspec/run_pspec_true_sum_PXLKT.sh
@@ -13,7 +13,7 @@ conda activate h1c_idr3_2_validation
 cd /lustre/aoc/projects/hera/Validation/test-4.1.0/LSTBIN/true_sum/all_epochs_preprocessed/pspec
 
 echo "start: $(date)"
-pspec_dir=/lustre/aoc/projects/hera/Validation/test-4.1.0/software/hera_pipelines/pipelines/validation/h1c_idr3_2/pspec/
+pspec_dir=/lustre/aoc/projects/hera/Validation/test-4.1.0/software/hera_pipelines/pipelines/validation/h1c_idr3_2/pspec
 ${pspec_dir}/pspec_pipe.py ${pspec_dir}/pspec_params_true_sum_PXLTK.yaml
 
 echo "end: $(date)"
