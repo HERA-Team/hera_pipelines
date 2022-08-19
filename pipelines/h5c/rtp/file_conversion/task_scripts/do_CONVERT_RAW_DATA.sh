3ddeb7bfff0fb9f9044e9325f910667beabf1c78
Paul La Plante <plaplant@sas.upenn.edu>
2022-08-19 14:47:18 +0000

    Add file conversion workflow

---
+++ pipelines/h5c/rtp/file_conversion/task_scripts/do_CONVERT_RAW_DATA.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# Parameters are set in the configuration file, here we define their positions,
+# which must be consistent with the config.
+# 1 - filename
+meta_fn="${1}"
+
+sum_fn_in="${meta_fn/meta.hdf5/sum.dat}"
+diff_fn_in="${meta_fn/meta.hdf5/diff.dat}"
+sum_fn_out="${sum_fn_in/dat/uvh5}"
+diff_fn_out="${diff_fn_in/dat/uvh5}"
+sum_cmd="hera_convert_uvh5.py -i $sum_fn_in -m $meta_fn -o $sum_fn_out"
+diff_cmd="hera_convert_uvh5.py -i $diff_fn_in -m $meta_fn -o $diff_fn_out"
+echo $sum_cmd
+eval $sum_cmd
+echo $diff_cmd
+eval $diff_cmd
+
+# add to M&C
+cmd="mc_add_observation.py $sum_fn_out"
+echo $cmd
+eval $cmd
+# add to RTP
+cmd="mc_rtp_launch_record.py $sum_fn_out"
+echo $cmd
+eval $cmd
