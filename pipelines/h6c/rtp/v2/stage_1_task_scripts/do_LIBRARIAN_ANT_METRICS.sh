7e894bf0e1ce337760c2bcb5d6d053d44b8adf6e
Josh Dillon <jsdillon@berkeley.edu>
2022-10-04 16:29:43 -0700

    hard-code ant_metrics ext

--- pipelines/h6c/rtp/v2/stage_1_task_scripts/do_LIBRARIAN_ANT_METRICS.sh
+++ pipelines/h6c/rtp/v2/stage_1_task_scripts/do_LIBRARIAN_ANT_METRICS.sh
@@ -10,13 +10,11 @@ source ${src_dir}/_common.sh
 # Parameters are set in the configuration file. Here we define their positions,
 # which must be consistent with the config.
 # 1 - filename
-# 2 - extension: Extension to be appended to the file name.
-# 3 - upload_to_librarian: global boolean trigger
-# 4 - librarian_ant_metrics: boolean trigger for this step
+# 2 - upload_to_librarian: global boolean trigger
+# 3 - librarian_ant_metrics: boolean trigger for this step
 fn="${1}"
-extension="${2}"
-upload_to_librarian="${3}"
-librarian_ant_metrics="${4}"
+upload_to_librarian="${2}"
+librarian_ant_metrics="${3}"
 
 bn=`basename ${fn}`
 jd=$(get_int_jd ${fn})
@@ -27,9 +25,9 @@ if [ "${upload_to_librarian}" == "True" ]; then
     if [ "${librarian_ant_metrics}" == "True" ]; then
 
         # Compress all ant_metrics files into one with a JD corresponding to $fn
-        compressed_file=`echo ${fn%.uvh5}${extension}.tar.gz`
-        echo tar czfv ${compressed_file} zen.${jd}*${extension}
-        tar czfv ${compressed_file} zen.${jd}*${extension}
+        compressed_file=`echo ${fn%.uvh5}.ant_metrics.hdf5.tar.gz`
+        echo tar czfv ${compressed_file} zen.${jd}*.ant_metrics.hdf5
+        tar czfv ${compressed_file} zen.${jd}*.ant_metrics.hdf5
 
         # Upload gzipped file to the librarian
         librarian_file=`basename ${compressed_file}`
