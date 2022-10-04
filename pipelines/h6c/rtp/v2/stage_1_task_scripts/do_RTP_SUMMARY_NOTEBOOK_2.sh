a0e7751e6cac5559ceab6f5813f9b4cad693ef0d
Josh Dillon <jsdillon@berkeley.edu>
2022-10-04 15:30:28 -0700

    remove outdated option

--- pipelines/h6c/rtp/v2/stage_1_task_scripts/do_RTP_SUMMARY_NOTEBOOK_2.sh
+++ pipelines/h6c/rtp/v2/stage_1_task_scripts/do_RTP_SUMMARY_NOTEBOOK_2.sh
@@ -6,11 +6,11 @@ set -e
 # Parameters are set in the configuration file. Here we define their positions,
 # which must be consistent with the config.
 # 1 - (raw) filename
-# 2 - ant_metrics_ext: extension for ant_metrics files to use
-# 3 - nb_template_dir: where to look for the notebook template
-# 4 - nb_output_repo: repository for saving evaluated notebooks
-# 5 - git_push: boolean whether to push the results created in the nb_output_repo
+# 2 - nb_template_dir: where to look for the notebook template
+# 3 - nb_output_repo: repository for saving evaluated notebooks
+# 4 - git_push: boolean whether to push the results created in the nb_output_repo
+# 5 - good_statuses: string list of comma-separated (no spaces) antenna statuses considered "good"
 
 src_dir="$(dirname "$0")"
-echo ${src_dir}/do_RTP_SUMMARY_NOTEBOOK_1.sh ${1} ${2} ${3} ${4} ${5} ${6}
-${src_dir}/do_RTP_SUMMARY_NOTEBOOK_1.sh ${1} ${2} ${3} ${4} ${5} ${6}
+echo ${src_dir}/do_RTP_SUMMARY_NOTEBOOK_1.sh ${1} ${2} ${3} ${4} ${5}
+${src_dir}/do_RTP_SUMMARY_NOTEBOOK_1.sh ${1} ${2} ${3} ${4} ${5}
