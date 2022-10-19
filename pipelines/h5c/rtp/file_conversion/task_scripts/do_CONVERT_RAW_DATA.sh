59e964e2d433d25afa369b12ff3ca9e95c828432
Josh Dillon <jsdillon@berkeley.edu>
2022-10-19 08:14:00 -0700

    add date prints

--- pipelines/h5c/rtp/file_conversion/task_scripts/do_CONVERT_RAW_DATA.sh
+++ pipelines/h5c/rtp/file_conversion/task_scripts/do_CONVERT_RAW_DATA.sh
@@ -15,16 +15,20 @@ sum_fn_out="${sum_fn_in/dat/uvh5}"
 diff_fn_out="${diff_fn_in/dat/uvh5}"
 sum_cmd="hera_convert_uvh5.py -i $sum_fn_in -m $meta_fn -o $sum_fn_out"
 diff_cmd="hera_convert_uvh5.py -i $diff_fn_in -m $meta_fn -o $diff_fn_out"
+date
 echo $sum_cmd
 eval $sum_cmd
+date
 echo $diff_cmd
 eval $diff_cmd
 
 # add to M&C
 cmd="mc_add_observation.py $sum_fn_out"
+date
 echo $cmd
 eval $cmd
 # add to RTP
 cmd="mc_rtp_launch_record.py $sum_fn_out"
+date
 echo $cmd
 eval $cmd
