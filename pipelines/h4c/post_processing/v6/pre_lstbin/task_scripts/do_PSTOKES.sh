78166bde178cae0c3fa1682887dc56afaaca7109
Aaron Ewall-Wice <aaronew@berkeley.edu>
2022-11-18 15:15:19 -0800

    Document PSTOKES.

--- pipelines/h4c/post_processing/v6/pre_lstbin/task_scripts/do_PSTOKES.sh
+++ pipelines/h4c/post_processing/v6/pre_lstbin/task_scripts/do_PSTOKES.sh
@@ -1,21 +1,47 @@
 #! /bin/bash
-set -e
 
-# import common functions
+#-----------------------------------------------------------------------------
+# This script computes psuedo-stokes (pstokes) visibilities as a power-spectrum
+# pre-processing step.
+#-----------------------------------------------------------------------------
+
+
+set -e
+# sometimes /tmp gets filled up on NRAO nodes hence this line.
+# haven't need to use it recently.
+#export TMPDIR=/lustre/aoc/projects/hera/heramgr/tmp/
+#import common functions
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
 
-# the args are
-# 1 - input file name.
-# 2 - label
-# 3 - group string identifier
-# 4 - pstokes to calculate
+#-----------------------------------------------------------------------------
+# ARGUMENTS
+# 1) fn: Input filename (string) assumed to contain JD.
+# 2) include_diffs: Whether or not to perform analysis on diff files as well as sum files.
+#    valid options are "true" or "false".
+# 3) label: identifying string label for analysis outputs to set it apart from other
+#    runs with different parameters.
+# 4) pstokes: tab separated list of pstokes labels to calculate. "pI pQ" for example.
+#    upstream throws away xx, yy by default so pI and pQ are the only ones that
+#    will typically work.
+#
+# ASSUMED INPUTS:
+# 1) stalk filtered, delay inpainted, time-averaged sum/diff files with
+#    XX and YY polarizations with naming format
+#    zen.${jd}.${sd}.${label}.${ext}.xtalk_filtered.tavg.uvh5
+# OUTPUTS:
+# 1) sum/diff xtalk filtered, delay inpainted, time-averaged sum/diff files
+#    with pStokes polarizations included. Naming convention is
+#    zen.${jd}.${sd}.${label}.${ext}.xtalk_filtered_pstokes.tavg.uvh5
+#
+#-----------------------------------------------------------------------------
+
+
 
 fn="${1}"
 include_diffs="${2}"
 label="${3}"
-grpstr="${4}"
-pstokes="${@:4}"
+pstokes="${@:3}"
 
 
 
