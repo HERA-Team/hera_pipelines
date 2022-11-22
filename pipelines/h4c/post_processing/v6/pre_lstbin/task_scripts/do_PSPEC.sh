588b71b2028ffe00b1d3ecc51fe64602f9839f1a
Aaron Ewall-Wice <aaronew@berkeley.edu>
2022-11-22 10:45:21 -0800

    document do_PSPEC

--- pipelines/h4c/post_processing/v6/pre_lstbin/task_scripts/do_PSPEC.sh
+++ pipelines/h4c/post_processing/v6/pre_lstbin/task_scripts/do_PSPEC.sh
@@ -1,23 +1,55 @@
 #! /bin/bash
-set -e
-#export TMPDIR=/lustre/aoc/projects/hera/heramgr/tmp/
 
-# import common functions
+#-----------------------------------------------------------------------------
+# This script computes night power spectra as part of the nightly post
+# processing pipeline.
+#-----------------------------------------------------------------------------
+
+
+set -e
+# sometimes /tmp gets filled up on NRAO nodes hence this line.
+# haven't need to use it recently.
+#export TMPDIR=/lustre/aoc/projects/hera/heramgr/tmp/
+#import common functions
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
 
-#the args are
-# 1 - input file name.
-# 2 - label
-# 3 - path to beam file
-# 4 - group string identifier.
-# 4 - spw-ranges to use fed as comma separated list with tildes separating upper / lower channels.
-# 5 - polarizatios to calculate fed as comma separated list with tildes separating lower / uppper channels.
+#-----------------------------------------------------------------------------
+# ARGUMENTS
+# 1) fn: Input filename (string) assumed to contain JD.
+# 2) include_diffs: Whether or not to perform analysis on diff files as well as sum files.
+#    valid options are "true" or "false".
+# 3) label: identifying string label for analysis outputs to set it apart from other
+#    runs with different parameters.
+# 4) beam_file_stem: string denoting location of beam file to use for normalization.
+#    basically the full beam path minus a potential polarizatin post-fix.
+#    example: /lustre/aoc/projects/hera/H4C/beams/NF_HERA_Vivaldi_efield_beam_healpix
+# 5) spw_ranges: comma seprated list of spw ranges for each pspec denoted by tildes.
+#    example: 10~105,150~320,515~615,665~717,770~1090
+
+# ASSUMED INPUTS:
+# 1) Xtalk filtered, delay inpainted, time-averaged sum/diff files with
+#    pstokes I polarizations with naming format
+#    zen.${jd}.${sd}.${label}.${ext}.xtalk_filtered${pol_label}.tavg.uvh5
+#    where pol_label can be "_pI" or "" (for XX/YY).
+
+# OUTPUTS:
+# 1) sum/diff xtalk filtered, delay inpainted, time-averaged pstokes power spectra
+#    with format
+#    zen.${jd}.${sd}.${label}.${ext}.xtalk_filtered${pol_label}.tavg.pspec.h5
+#
+#
+#-----------------------------------------------------------------------------
+
+
+
 fn="${1}"
 include_diffs="${2}"
 label="${3}"
 beam_file_stem="${4}"
 spw_ranges="${5}"
+
+
 
 jd=$(get_jd $fn)
 int_jd=${jd:0:7}
