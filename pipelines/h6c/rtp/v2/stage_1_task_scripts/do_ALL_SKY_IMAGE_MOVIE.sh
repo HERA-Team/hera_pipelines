52bf43df1b7b9839873299b2f64420985bc3095f
Danny Jacobs <daniel.c.jacobs@asu.edu>
2023-01-24 11:42:26 -0700

    bringing in changes from testing. works on site

--- pipelines/h6c/rtp/v2/stage_1_task_scripts/do_ALL_SKY_IMAGE_MOVIE.sh
+++ pipelines/h6c/rtp/v2/stage_1_task_scripts/do_ALL_SKY_IMAGE_MOVIE.sh
@@ -5,15 +5,17 @@ set -e
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
 
-
 # the only input we need is the filename
 fn=${1}
+CASA_ENV=${2}
 
 # Get the integer JD from filename
 jd=$(get_int_jd ${fn})
+#put all the jpgs into a sub-dir for easy stacking with a ffmpeg glob
+mkdir -p ${jd}_imgs
+mv *${jd}*jpg ${jd}_imgs
 
-
-
+conda activate $CASA_ENV
 #image stack is in its own dir because glob dereferencing is hard
 cd ${jd}_imgs
 ffmpeg -f image2 -pattern_type glob -i '*.jpg' HERA_${jd}_automatic.mp4
--- pipelines/h6c/rtp/v2/stage_1_task_scripts/do_ALL_SKY_IMAGE_SNAPSHOT.sh
+++ pipelines/h6c/rtp/v2/stage_1_task_scripts/do_ALL_SKY_IMAGE_SNAPSHOT.sh
@@ -1,37 +1,43 @@
 #! /bin/bash
 set -e
 
-
+echo "do_ALL_SKY_IMAGE_SNAPSHOT"
+hostname
 #This script generates an all-sky image of the sky as a jpg
 # a subsequent job collects them all into an image
 
-
 # for now assumes scripts are to be found in ~/bin/
+
+
 
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
-
 # Parameters are set in the configuration file. Here we define their positions,
 # which must be consistent with the config.
 fn=${1}
+UVDATA_ENV="${2}"
+CASA_ENV="${3}"
 
 # Export variables used by the notebook
 export SUM_FILE="$(cd "$(dirname "$fn")" && pwd)/$(basename "$fn")"
 #CALFILE=/lustre/aoc/projects/hera/H6C/2459853/zen.2459853.35006.sum.omni.calfits
 
-
+echo "conda activate $UVDATA_ENV"
 #take a raw HERA snapshot to widefield image and render it as a jpg
-mamba activate HERA2023
+conda activate $UVDATA_ENV
 #step 1 make a calibrated MS.  read raw, apply cal, write ms
 # comment --cal_file to LET CAL FLOAT
+echo "python ~/bin/uvh5tocalibratedms.py ${SUM_FILE}"
 python ~/bin/uvh5tocalibratedms.py ${SUM_FILE} # --cal_file ${CALFILE}
 
 #step 2 image the ms
 ## 2.1 use my awesome casa env
-mamba activate casa_py3.8
+echo "conda activate $CASA_ENV"
+conda activate $CASA_ENV
 ## 2.2 fung up the name of the file I made above
 MSFILE=$(python -c "import os; print(os.path.basename('${SUM_FILE}').replace('uvh5','ms'))")
 ## 2.3 time to synth-e-size
+echo "python ~/bin/hera_widefield_image.py  $MSFILE "
 python ~/bin/hera_widefield_image.py  $MSFILE 
 
 #step 3 render to a jpg
@@ -39,16 +45,16 @@ python ~/bin/hera_widefield_image.py  $MSFILE
 IMAGEFILE=$(python -c "import os; print('${MSFILE}'.replace('ms','v1.fits'))")
 echo "IMAGEFILE:" ${IMAGEFILE}
 # 3.2 get back to my bleedin edge python3.10 env
-mamba activate HERA2023
+conda activate $UVDATA_ENV
 # 3.3 make killer jpg
 python ~/bin/widefield_fits2img.py $IMAGEFILE
 
 #CLEANUP
 #put all the jpgs into a sub-dir for easy stacking later
-jd=$(get_int_jd ${fn})
-mkdir ${jd}_imgs
-mv *${jd}*jpg ${jd}_imgs
+#jd=$(get_int_jd ${fn})
+#mkdir ${jd}_imgs
+#mv *${jd}*jpg ${jd}_imgs
 
 # delete ms, v1
-rm -rf $MSFILE
+#rm -rf $MSFILE
 
