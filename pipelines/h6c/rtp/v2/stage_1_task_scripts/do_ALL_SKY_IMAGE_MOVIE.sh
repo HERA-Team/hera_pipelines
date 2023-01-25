81266291dfe91174382578f8d1731c3d75f4bbf2
Josh Dillon <jsdillon@berkeley.edu>
2023-01-25 10:44:40 -0800

    move conda initalize

--- pipelines/h6c/rtp/v2/stage_1_task_scripts/do_ALL_SKY_IMAGE_MOVIE.sh
+++ pipelines/h6c/rtp/v2/stage_1_task_scripts/do_ALL_SKY_IMAGE_MOVIE.sh
@@ -5,6 +5,25 @@ set -e
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
 
+# >>> conda initialize >>>
+# contents within this block must be kept matching those in ~/.bashrc
+__conda_setup="$('/home/obs/mambaforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/obs/mambaforge/etc/profile.d/conda.sh" ]; then
+        . "/home/obs/mambaforge/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/obs/mambaforge/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+if [ -f "/home/obs/mambaforge/etc/profile.d/mamba.sh" ]; then
+    . "/home/obs/mambaforge/etc/profile.d/mamba.sh"
+fi
+# <<< conda initialize <<<
+
 # the only input we need is the filename
 fn=${1}
 CASA_ENV=${2}
--- pipelines/h6c/rtp/v2/stage_1_task_scripts/do_ALL_SKY_IMAGE_SNAPSHOT.sh
+++ pipelines/h6c/rtp/v2/stage_1_task_scripts/do_ALL_SKY_IMAGE_SNAPSHOT.sh
@@ -8,10 +8,28 @@ hostname
 
 # for now assumes scripts are to be found in ~/bin/
 
-
-
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
+
+# >>> conda initialize >>>
+# contents within this block must be kept matching those in ~/.bashrc
+__conda_setup="$('/home/obs/mambaforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/obs/mambaforge/etc/profile.d/conda.sh" ]; then
+        . "/home/obs/mambaforge/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/obs/mambaforge/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+if [ -f "/home/obs/mambaforge/etc/profile.d/mamba.sh" ]; then
+    . "/home/obs/mambaforge/etc/profile.d/mamba.sh"
+fi
+# <<< conda initialize <<<
+
 # Parameters are set in the configuration file. Here we define their positions,
 # which must be consistent with the config.
 fn=${1}
