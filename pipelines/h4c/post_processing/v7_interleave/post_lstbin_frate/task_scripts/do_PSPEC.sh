f5cd5d14cb74d2fc9b947f038f23b6cdee9c8f54
Aaron <aaronew@alum.mit.edu>
2023-03-18 15:11:49 -0700

    added interleaving to PSPEC script.

--- pipelines/h4c/post_processing/v7_interleave/post_lstbin_frate/task_scripts/do_PSPEC.sh
+++ pipelines/h4c/post_processing/v7_interleave/post_lstbin_frate/task_scripts/do_PSPEC.sh
@@ -12,6 +12,7 @@ include_diffs="${2}"
 label="${3}"
 beam_file_stem="${4}"
 spw_ranges="${5}"
+ninterleave="${6}"
 
 jd=$(get_jd $fn)
 int_jd=${jd:0:7}
@@ -34,6 +35,16 @@ exts=("frf" "foreground_filled.xtalk_filtered.chunked" )
 pol_pair_list=("XX~XX,YY~YY" "pI~pI")
 pol_label_list=("" "_pstokes")
 polnums=(0 1)
+# Create dset_pairs string.
+dset_pairs_str = ""
+for interleave_1 in {0...$((${ninterleave} - 1))}
+do
+    for interleave_2 in {${interleave_1}...$((${ninterleave} - 1))}
+    do
+	dset_pairs_str = "${dset_pairs_str},${interleave_1}~${interleave_2}"
+    done
+done
+
 for sd in ${sumdiff[@]}
 do
   for ext in ${exts[@]}
@@ -44,21 +55,25 @@ do
       pol_label=${pol_label_list[$polnum]}
       beam_file=${beam_file_stem}${pol_label}.fits
       # power spectra of cross-talk filtered data.
-
-      input=zen.${jd}.${sd}.${label}.${ext}${pol_label}.tavg.uvh5
+      # Generate list of dsets (interleaves).
+      inputs = ""
+      for interleave in {0...$((${ninterleave} - 1))}
+      do
+	  inputs="${inputs} zen.${jd}.${sd}.${label}.${ext}${pol_label}.tavg.interleave_${interleave}uvh5"
+      done
       if [ -e "${input}" ]
       then
           output=zen.${jd}.${sd}.${label}.${ext}${pol_label}.tavg.pspec.h5
           # average all times incoherently
-          echo pspec_run.py ${input} ${output}\
-            --overwrite\
+          echo pspec_run.py ${inputs} ${output}\
+            --overwrite --dset_pairs ${dset_pairs_str}\
             --pol_pairs ${pol_pairs} --verbose\
             --Jy2mK --beam ${beam_file} --exclude_permutations\
             --file_type uvh5 --xant_flag_thresh 1.1\
             --taper bh --spw_ranges ${spw_ranges}
 
-            pspec_run.py ${input} ${output}\
-              --overwrite\
+            pspec_run.py ${inputs} ${output}\
+              --overwrite --dset_pairs ${dset_pairs_str}\
               --pol_pairs ${pol_pairs} --verbose\
               --Jy2mK --beam ${beam_file} --exclude_permutations\
               --file_type uvh5  --xant_flag_thresh 1.1\
@@ -68,8 +83,8 @@ do
             then
               # auto power spectra
               output=zen.${jd}.${sd}.${label}.autos.${ext}${pol_label}.tavg.pspec.h5
-              echo pspec_run.py ${input} ${output}\
-                --overwrite\
+              echo pspec_run.py ${inputs} ${output}\
+                --overwrite --dset_pairs ${dset_pairs_str}\
                 --pol_pairs ${pol_pairs} --verbose\
                 --Jy2mK --beam ${beam_file}\
                 --file_type uvh5 --include_autocorrs --xant_flag_thresh 1.1\
@@ -78,7 +93,7 @@ do
 
 
               pspec_run.py ${input} ${output}\
-                --overwrite\
+                --overwrite --dset_pairs ${dset_pairs_str}\
                 --pol_pairs ${pol_pairs} --verbose\
                 --Jy2mK --beam ${beam_file}\
                 --file_type uvh5 --include_autocorrs --xant_flag_thresh 1.1\
