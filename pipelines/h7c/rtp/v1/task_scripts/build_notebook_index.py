a06d151849301d3b81efc63b672f665de6cde2d3
Josh Dillon <jsdillon@berkeley.edu>
2023-07-15 15:19:05 -0700

    get correct relative path

--- pipelines/h7c/rtp/v1/task_scripts/build_notebook_index.py
+++ pipelines/h7c/rtp/v1/task_scripts/build_notebook_index.py
@@ -9,7 +9,7 @@ import re
 import glob
 from astropy.time import Time
 
-# Parse arguments and move to target_directory
+# Parse arguments
 a = argparse.ArgumentParser(
     description='Script for building an index.html that links to files in this folder.'
 )
@@ -30,7 +30,7 @@ def make_links(files):
         if len(JD_strs) > 0:
             utc = Time(JD_strs[-1], format='jd').datetime
             date_str = f' ({utc.year}-{utc.month}-{utc.day})'
-        links.append(f'    <li><a href="{file}">{file}{date_str}</a></li>')
+        links.append(f'    <li><a href="{file}">{file.split("/")[-1]}{date_str}</a></li>')
     return links
 
 links = make_links(files)
@@ -41,13 +41,16 @@ with open(os.path.join(args.target_dir, 'index.html'), 'w') as f:
     f.write('\n</ul>\n</body>\n</html>')
 
 
-all_html_files = glob.glob(os.path.join(args.target_dir, "../*/*.html"))
+starting_dir = os.getcwd()
+os.chdir(os.path.join(args.target_dir, '..'))
+
+all_html_files = glob.glob(os.path.join(args.target_dir, "*/*.html"))
 mod_times = [os.path.getmtime(f) for f in all_html_files]
 file_time_pairs = list(zip(all_html_files, mod_times))
 recent_html_files = [pair[0] for pair in sorted(file_time_pairs, key=lambda x: x[1], reverse=True)[0:20]]
 
 links = make_links(recent_html_files)
-overall_index = '<html>\n<title>H7C_Notebooks</title>\n<header>\n<h1>H7C_Notebooks</h1>\n</header>\n<body>\n<ul>\n'
+overall_index = '<html>\n<title>H7C_Notebooks</title>\n<header>\n<h1>H7C_Notebooks</h1>\n</header>\n<body>\n<h3><ul>\n'
 overall_index += '    <li><a href=".."><b>Back to all seasons.</b></a></li>\n'
 overall_index += '    <li><a href="antenna_classification_summary">antenna_classification_summary</a></li>\n'
 overall_index += '    <li><a href="calibration_smoothing">calibration_smoothing</a></li>\n'
@@ -56,9 +59,12 @@ overall_index += '    <li><a href="file_calibration">file_calibration</a></li>\n
 overall_index += '    <li><a href="full_day_antenna_flagging">full_day_antenna_flagging</a></li>\n'
 overall_index += '    <li><a href="full_day_auto_checker">full_day_auto_checker</a></li>\n'
 overall_index += '    <li><a href="full_day_rfi">full_day_rfi</a></li>\n'
-overall_index += '<h2>Most Recent Notebooks:</h2>\n<ul>\n'
+overall_index += '</ul>\n</h3>\n<h2>Most Recent Notebooks:</h2>\n<ul>\n'
 overall_index += '\n'.join(links)
 overall_index += "\n</ul>\n</body>\n</html>"
 
 with open(os.path.join(args.target_dir, '../index.html'), 'w') as f:
     f.write(overall_index)
+
+# move back to starting location
+os.chdir(starting_dir)
