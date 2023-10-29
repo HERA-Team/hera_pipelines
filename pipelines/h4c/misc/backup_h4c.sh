76d06a7da82a1b4fa3516ad6184dfe9b60962748
Aaron <aaronew@alum.mit.edu>
2023-10-29 17:24:42 -0400

    add backup script.

--- pipelines/h4c/misc/backup_h4c.sh
+++ pipelines/h4c/misc/backup_h4c.sh
@@ -2,4 +2,4 @@
 # Script for backing up H4C data products.
 
 # Backup all lstbinned data.
-rsync -r /lustre/aoc/projects/hera/H4C/posprocessing/lstbin herastore01:/export/hera/herastore01-2/H4C/
+rsync -rv /lustre/aoc/projects/hera/H4C/posprocessing/lstbin herastore01:/export/hera/herastore01-2/H4C/
