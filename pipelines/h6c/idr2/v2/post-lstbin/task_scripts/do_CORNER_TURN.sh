90fc85ec6337f4648b40802f5632eeb14a45b6d8
Josh Dillon <jsdillon@berkeley.edu>
2024-01-22 11:37:54 -0800

    Create do_CORNER_TURN.sh

---
+++ pipelines/h6c/idr2/v2/post-lstbin/task_scripts/do_CORNER_TURN.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+set -e
+
+# This loads a single baseline (both pols) for all times and writes it as its own file.
+
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+echo Host: `hostname`
+
+fn=${1}
+out_folder=$(dirname "$fn")/single_baseline_files
+file_glob=`echo $(dirname "$fn")/zen.LST.*`
+
+echo corner_turn.py ${fn} ${out_folder} ${file_glob}
+corner_turn.py ${fn} ${out_folder} ${file_glob}
