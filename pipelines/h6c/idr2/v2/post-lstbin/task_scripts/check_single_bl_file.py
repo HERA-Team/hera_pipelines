15eef7c014b4ca917943e6a81c1d45ef21e3635c
Josh Dillon <jsdillon@berkeley.edu>
2024-02-02 12:39:27 -0800

    Create check_single_bl_file.py

---
+++ pipelines/h6c/idr2/v2/post-lstbin/task_scripts/check_single_bl_file.py
@@ -0,0 +1,23 @@
+import argparse
+from hera_cal import io
+import numpy as np
+
+# argparser for getting path to file to check
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="single baseline file to check, raises an error if either polarization is completely flagged")
+parser.add_argument("--skip_autos", action='store_true', help="also raises an error if the file contains only autocorrelations")
+args = parser.parse_args()
+
+# read in file
+hd = io.HERAData(args.file)
+_, flags, _ = hd.read()
+
+# check if all baselines are autocorrelaitons
+if args.skip_autos:
+    if np.all([bl[0] == bl[1] for bl in flags]):
+        raise ValueError("This file has only autocorrelations and so we shouldn't use it for power spectrum estimation.")
+
+# check if either polarization is completely flagged
+for pol in hd.pols:
+    if np.all([flags[bl] for bl in flags if bl[2] == pol]):
+        raise ValueError(f"All baselines for pol {pol} are flagged, so it isn't appropriate for power spectrum estimation.")
