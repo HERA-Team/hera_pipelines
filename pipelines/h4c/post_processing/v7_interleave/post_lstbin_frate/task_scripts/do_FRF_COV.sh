3123e1edf28b948399b3b45f207c33aa0f987b45
Mike Wilensky <m.wilensky@qmul.ac.uk>
2024-05-10 14:02:02 +0100

    change jd handling

--- pipelines/h4c/post_processing/v7_interleave/post_lstbin_frate/task_scripts/do_FRF_COV.sh
+++ pipelines/h4c/post_processing/v7_interleave/post_lstbin_frate/task_scripts/do_FRF_COV.sh
@@ -19,8 +19,10 @@ jd=$(get_jd $fn)
 int_jd=${jd:0:7}
 if [[ "$int_jd" == *"."* ]]; then
   jd=`echo ${fn} | grep -o "[0-9]\{1,2\}.[0-9]\{5\}"`
+  int_jd="LST"
   jd="LST.${jd}"
 fi
+
 
 # split up spw ranges.
 spw_ranges="${spw_ranges//,/$' '}"
@@ -35,15 +37,15 @@ else
 fi
 
 
-baseline_chunk_files=`echo zen.${int_jd}.*.sum.${label}.frf.waterfall.uvh5`
-if echo x"$baseline_chunk_files" | grep '*' > /dev/null; then
+waterfall_files=`echo zen.${int_jd}.*.sum.${label}.frf.waterfall.uvh5`
+if echo x"$waterfall_files" | grep '*' > /dev/null; then
 		echo "No waterfall files exist with ${jd}. This is probably because there are more times than baseline groups."
 else
     echo ${ilabel}
     # Calculate FRF cov
     fn_out=zen.${jd}.sum.${label}.frf.tavg.cov.interleave_${ilabel}.npy
 	
-    cmd="FRF_noise_cov_run.py ${baseline_chunk_files}  --tol ${tol} \
+    cmd="FRF_noise_cov_run.py ${waterfall_files}  --tol ${tol} \
       --fn_out ${fn_out} --clobber --verbose --spw_range ${spw_range} \
       --ninterleave ${ninterleave} ${pf_arg}"		   
     echo ${cmd}
