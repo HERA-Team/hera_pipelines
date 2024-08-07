2c0f67406eb0908f6e43cbc4f84e3c4440de15bf
Josh Dillon <jsdillon@berkeley.edu>
2024-08-07 14:09:02 -0700

    update corner turner for new separated auto files

--- pipelines/h6c/idr2/v3/post-lstbin/task_scripts/corner_turn.py
+++ pipelines/h6c/idr2/v3/post-lstbin/task_scripts/corner_turn.py
@@ -1,6 +1,7 @@
 import yaml
 import glob
 from hera_cal import io
+from pyuvdata import FastUVH5Meta
 import os
 import argparse
 
@@ -10,49 +11,75 @@ parser.add_argument("this_file", help="this particular file, used to index into 
 parser.add_argument("out_folder", help="output folder")
 args = parser.parse_args()
 
+# the cornerturn makeflow should only be built on cross-baseline LST-binned files
+if 'autos' in args.this_file:
+    raise ValueError(f'{args.this_file} is an autocorrelation file, not a cross-correlation file.')
+
 # create out_folder if it doesn't exist
 if not os.path.exists(args.out_folder):
     os.makedirs(args.out_folder)
 
+# get files and split into auto and cross files
 glob_str = '.'.join(['*' if part.isdigit() else part for part in os.path.basename(args.this_file).split('.')])
 all_files = sorted(glob.glob(os.path.join(os.path.dirname(args.this_file), glob_str)))
-assert args.this_file in all_files, f'{args.this_file} not in {all_files}'
-out_yaml = os.path.join(os.path.dirname(args.this_file), 'file_to_baseline_map.yaml')
-hd = None
+auto_files = sorted([f for f in all_files if 'autos' in f.split('/')[-1]])
+cross_files = sorted([f for f in all_files if 'autos' not in f.split('/')[-1]])
+
+# make sure this file is in all_files
+if args.this_file not in all_files:
+    raise ValueError(f'{args.this_file} not in {os.path.join(os.path.dirname(args.this_file), glob_str)}')
+
+# if necessary, create yaml mapping cross files to cross antpairs
+out_yaml = os.path.join(os.path.dirname(args.this_file), 'cross_files_to_antpairs_map.yaml')
 if os.path.exists(out_yaml):
     # read in yaml file if it exists
     with open(out_yaml, 'r') as file:
-        files_to_bls_map = yaml.unsafe_load(file)
+        cross_files_to_aps_map = yaml.unsafe_load(file)
 else:
-    # create yaml file if it doesn't exist
-    hd = io.HERAData(all_files)
-    pols = set([bl[2] for bls in hd.bls.values() for bl in bls])
-    bls = sorted(set([bl[0:2] for bls in hd.bls.values() for bl in bls]))
-    files_to_bls_map = {file: [] for file in all_files}
-    for i, bl in enumerate(bls):
-        files_to_bls_map[all_files[i % len(all_files)]].append([bl + (pol,) for pol in pols])
+    # create lists of FastUVH5Meta objects
+    auto_bl_metas = [FastUVH5Meta(f) for f in auto_files]
+    cross_bl_metas = [FastUVH5Meta(f) for f in cross_files]
+
+    # get pols and antpairs from metas
+    auto_bl_pols = set([p for meta in auto_bl_metas for p in meta.pols])
+    cross_bl_pols = set([p for meta in cross_bl_metas for p in meta.pols])
+    auto_bl_antpairs = set([ap for meta in auto_bl_metas for ap in meta.antpairs])
+    cross_bl_antpairs = set([ap for meta in cross_bl_metas for ap in meta.antpairs])
+
+    # sense checks
+    assert sorted(auto_bl_pols) == sorted(cross_bl_pols)
+    assert len(auto_bl_antpairs) == 1
+
+    # create cross_files_to_aps_map
+    cross_files_to_aps_map = {file: [] for file in cross_files}
+    cross_files_to_aps_map[cross_files[0]].append(list(auto_bl_antpairs)[0])
+    for i, ap in enumerate(cross_bl_antpairs):
+        cross_files_to_aps_map[cross_files[(i + 1) % len(cross_files)]].append(ap)
     
     # write file and try to avoid collisions/race conditions
     if not os.path.exists(out_yaml):
         with open(args.this_file + '_temp_yaml', 'w') as file:
-            yaml.dump(files_to_bls_map, file)
+            yaml.dump(cross_files_to_aps_map, file)
         os.rename(args.this_file + '_temp_yaml', out_yaml)
     
 # load all baselines corresponding to this_file and then write them out to uvh5
-if len(files_to_bls_map[args.this_file]) > 0:
-    for bl_pair in files_to_bls_map[args.this_file]:
-        outfile = os.path.join(args.out_folder, f'zen.LST.baseline.{bl_pair[0][0]}_{bl_pair[0][1]}.sum.uvh5')
-        if hd is None:
-            hd = io.HERAData(all_files)
-        print(f'Now working on {bl_pair}')
-        usable_files = [f for f in hd.filepaths if all([bl in hd.bls[f] for bl in bl_pair])]
-        if len(usable_files) < len(all_files):
-            print(f'Only {len(usable_files)} out of {len(all_files)} files have {bl_pair}')
-            hd = io.HERAData(usable_files)
-        hd.read(bls=bl_pair, return_data=False, axis='blt')
+if len(cross_files_to_aps_map[args.this_file]) > 0:
+    for antpair in cross_files_to_aps_map[args.this_file]:
+        # Read for this antpair
+        print(f'Now working on {antpair}')
+        if antpair[0] == antpair[1]:
+            hd = io.HERAData(auto_files)
+        else:
+            hd = io.HERAData(cross_files)
+            usable_files = [f for f in hd.filepaths if antpair in hd.antpairs[f]]
+            if len(usable_files) < len(cross_files):
+                print(f'Only {len(usable_files)} out of {len(cross_files)} files have {antpair}')
+                hd = io.HERAData(usable_files)
+        hd.read(bls=[antpair], return_data=False, axis='blt')
+        
+        # Write data
+        outfile = os.path.join(args.out_folder, f'zen.LST.baseline.{antpair[0]}_{antpair[1]}.sum.uvh5')
         print(f'\tWriting {outfile}')
         hd.write_uvh5(outfile, clobber=True)
-        if len(usable_files) < len(all_files):
-            hd = None # forces reinitialization for the next bl_pair
 else:
     print(f'No baselines correspond to {args.this_file}')
