6bb05a7f5888be86fc6071fec697b7c0f80fdb58
Danny Jacobs <wheresmytab@gmail.com>
2024-10-16 08:54:46 -0700

    Update do_ALL_SKY_IMAGE_MOVIE.sh
    Stop moving movie frames to a directory. Make movies from a more specific glob instead

--- pipelines/h8c/rtp/v1/task_scripts/do_ALL_SKY_IMAGE_MOVIE.sh
+++ pipelines/h8c/rtp/v1/task_scripts/do_ALL_SKY_IMAGE_MOVIE.sh
@@ -32,14 +32,10 @@ CASA_ENV=${2}
 
 # Get the integer JD from filename
 jd=$(get_int_jd ${fn})
-#put all the jpgs into a sub-dir for easy stacking with a ffmpeg glob
-mkdir -p ${jd}_imgs
-mv *${jd}*jpg ${jd}_imgs
 
 conda activate $CASA_ENV
-#image stack is in its own dir because glob dereferencing is hard
-cd ${jd}_imgs
-ffmpeg -f image2 -pattern_type glob -i '*.jpg' HERA_${jd}_automatic.mp4
+
+ffmpeg -f image2 -pattern_type glob -i 'zen*v1.jpg' HERA_${jd}_automatic.mp4
 cp  HERA_${jd}_automatic.mp4 ..
 # TODO put movie in Librarian and upload to enterprise for quick online viewing
 # Put movie where it will be rsynced to heranow by a regular cron
