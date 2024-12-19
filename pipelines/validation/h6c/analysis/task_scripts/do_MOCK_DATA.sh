9bf8de34fd5deb2058fc12c028dc7b1776d256e6
Josh Dillon <jsdillon@berkeley.edu>
2024-12-19 13:35:11 -0800

    update MOCK_DATA for h6c_idr2

--- pipelines/validation/h6c/analysis/task_scripts/do_MOCK_DATA.sh
+++ pipelines/validation/h6c/analysis/task_scripts/do_MOCK_DATA.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
+#! /bin/bash
 set -e
 
-echo "sourcing setup..."
-echo $PATH
-
-src_dir="$(dirname -- "$0")"
+# import common functions
+src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
-
-#source ~/.bashrc
-
-source /lustre/aoc/projects/hera/heramgr/anaconda3/etc/profile.d/conda.sh
-conda activate h4c_validation
 
 echo "reading in inputs..."
 
@@ -20,12 +13,12 @@ sky_cmp="${2}" # simulation type (e.g. diffuse, eor, point_src) must match name 
 
 jd=$(get_int_jd ${reffile})
 echo "jd is ${jd}"
-ref_path=/lustre/aoc/projects/hera/H4C/${jd}/${reffile}
-outdir=/lustre/aoc/projects/hera/Validation/H4C/IDR2/${jd}/
+ref_path=/lustre/aoc/projects/hera/h6c-analysis/IDR2/${jd}/${reffile}
+outdir=/lustre/aoc/projects/hera/Validation/H6C_IDR/${jd}/
 #outdir=/lustre/aoc/projects/hera/Validation/H4C/IDR2/mocked_data/${sky_cmp}/${jd}/
 #mkdir -p ${outdir} # the -p only makes dir it not existing
 
-config_file=/lustre/aoc/projects/hera/Validation/H4C/IDR2/configs/${jd}.yaml
+config_file=/lustre/aoc/projects/hera/Validation/H6C_IDR2/src/hera_pipelines/pipelines/validation/h6c/configs/${jd}.yaml
 
 echo "mocking up data for ${sky_cmp} sims..."
 echo "==============================================="
@@ -38,7 +31,7 @@ echo "using config file: ${config_file}"
 # reffile=$(basename ${f});
 # echo ${name};
 echo "using ${reffile}";
-echo "python /users/heramgr/hera_software/hera_pipelines/pipelines/validation/h4c/rtp/v2/stage_2_task_scripts/mock_data.py ${ref_path} ${sky_cmp} --config $config_file --sim_dir /lustre/aoc/projects/hera/Validation/H4C/IDR2/chunked_data/ -o ${outdir} --lst_wrap 3.1419561 --input_is_compressed --inflate --clobber"
-python /users/heramgr/hera_software/hera_pipelines/pipelines/validation/h4c/rtp/v2/stage_2_task_scripts/mock_data.py ${ref_path} ${sky_cmp} --config $config_file --sim_dir /lustre/aoc/projects/hera/Validation/H4C/IDR2/chunked_data/ -o ${outdir} --lst_wrap 3.1419561 --input_is_compressed --inflate --clobber
+echo "python /lustre/aoc/projects/hera/Validation/H6C_IDR2/src/hera_pipelines/pipelines/validation/h6c/analysis/task_scripts/mock_data.py ${ref_path} ${sky_cmp} --config $config_file --sim_dir /lustre/aoc/projects/hera/Validation/H4C/IDR2/chunked_data/ -o ${outdir} --lst_wrap 3.1419561 --input_is_compressed --inflate --clobber"
+python /lustre/aoc/projects/hera/Validation/H6C_IDR2/src/hera_pipelines/pipelines/validation/h6c/analysis/task_scripts/mock_data.py ${ref_path} ${sky_cmp} --config $config_file --sim_dir /lustre/aoc/projects/hera/Validation/H4C/IDR2/chunked_data/ -o ${outdir} --lst_wrap 3.1419561 --input_is_compressed --inflate --clobber
 echo "saved file to ${outdir}"
 
