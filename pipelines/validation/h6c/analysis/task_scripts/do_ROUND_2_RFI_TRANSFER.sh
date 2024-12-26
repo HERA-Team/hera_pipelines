b023b1ce00546a30da558ef730a6583c51c8507a
Josh Dillon <jsdillon@berkeley.edu>
2024-12-26 14:24:04 -0800

    fix path

--- pipelines/validation/h6c/analysis/task_scripts/do_ROUND_2_RFI_TRANSFER.sh
+++ pipelines/validation/h6c/analysis/task_scripts/do_ROUND_2_RFI_TRANSFER.sh
@@ -5,9 +5,7 @@ set -e
 src_dir="$(dirname "$0")"
 source ${src_dir}/_common.sh
 
-echo "reading in inputs..."
-
 fn="${1}" # sum file
 
-echo python ${src_dir}/round_2_rfi_transfer.py ${fn%.sum.uvh5}.smooth.calfits ${fn%.sum.uvh5}.flag_waterfall_round_2.h5
-python ${src_dir}/round_2_rfi_transfer.py ${fn%.sum.uvh5}.smooth.calfits ${fn%.sum.uvh5}.flag_waterfall_round_2.h5
+echo python ${src_dir}/round_2_rfi_transfer.py ${fn%.sum.uvh5}.sum.smooth.calfits ${fn%.sum.uvh5}.sum.flag_waterfall_round_2.h5
+python ${src_dir}/round_2_rfi_transfer.py ${fn%.sum.uvh5}.sum.smooth.calfits ${fn%.sum.uvh5}.sum.flag_waterfall_round_2.h5
