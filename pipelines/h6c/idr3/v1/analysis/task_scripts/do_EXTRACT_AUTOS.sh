c663abc039cb017ad2b01b254dcf71c3ad64435b
Josh Dillon <jsdillon@berkeley.edu>
2025-02-25 14:22:00 -0800

    start idr3.1 pipeline

---
+++ pipelines/h6c/idr3/v1/analysis/task_scripts/do_EXTRACT_AUTOS.sh
@@ -0,0 +1,74 @@
+#! /bin/bash
+set -e
+
+# import common functions
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+# Parameters are set in the configuration file, here we define their positions,
+# which must be consistent with the config.
+# 1 - filename
+# 2 - upload_to_librarian: global boolean trigger
+# 3 - librarian_autos: boolean trigger for this step
+fn="${1}"
+upload_to_librarian="${2}"
+librarian_autos="${3}"
+
+# get outfilename, removing extension and appending .autos.uvh5
+autos_file=`echo ${fn%.sum.uvh5}.sum.autos.uvh5`
+
+if [ -f "$autos_file" ]; then
+    echo ${autos_file} already exists... skipping extraction.
+else
+    echo extract_autos.py ${fn} ${autos_file} --clobber
+    extract_autos.py ${fn} ${autos_file} --clobber
+    echo Finished extracting autos from sum data at $(date)
+fi
+
+# now do the same for the diffs
+diff_file=`echo ${fn%.sum.uvh5}.diff.uvh5`
+diff_autos_file=`echo ${fn%.sum.uvh5}.diff.autos.uvh5`
+
+if [ -f "$diff_autos_file" ]; then
+    echo ${diff_autos_file} already exists... skipping extraction.
+else
+    if [ -f "$diff_file" ]; then
+        echo extract_autos.py ${diff_file} ${diff_autos_file} --clobber
+        extract_autos.py ${diff_file} ${diff_autos_file} --clobber
+        echo Finished extracting autos from diff data at $(date)
+    else
+        echo ${diff_file} does not exist... skipping extraction.
+    fi
+fi
+
+# now delete .dat files, since we've already shown that they are readable
+sum_dat_file=`echo ${fn%.sum.uvh5}.sum.dat`
+if [ -f "$sum_dat_file" ]; then
+    if [ -f "$fn" ]; then
+        echo Removing ${sum_dat_file}
+        rm -rf ${sum_dat_file}
+    fi
+fi
+diff_dat_file=`echo ${fn%.sum.uvh5}.diff.dat`
+if [ -f "$diff_dat_file" ]; then
+    if [ -f "$diff_file" ]; then
+        echo Removing ${diff_dat_file}
+        rm -rf ${diff_dat_file}
+    fi
+fi
+
+# upload to librarian
+if [ "${upload_to_librarian}" == "True" ]; then
+    if [ "${librarian_autos}" == "True" ]; then
+        # get the integer portion of the JD
+        jd=$(get_int_jd ${fn})
+
+        echo librarian upload local-rtp ${autos_file} ${jd}/${autos_file}
+        librarian upload local-rtp ${autos_file} ${jd}/${autos_file}
+        echo Finished uploading sum autos to Librarian at $(date)
+
+        echo librarian upload local-rtp ${diff_autos_file} ${jd}/${diff_autos_file}
+        librarian upload local-rtp ${diff_autos_file} ${jd}/${diff_autos_file}
+        echo Finished uploading diff autos to Librarian at $(date)
+    fi
+fi
---
+++ pipelines/h6c/idr3/v1/lstbin/task_scripts/do_PER_OUTFILE_LSTSTACK_METRICS_NOTEBOOK.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+export TMPDIR=/lustre/aoc/projects/hera/heramgr/tmp/
+
+# import common functions
+src_dir="$(dirname "$0")"
+source ${src_dir}/_common.sh
+
+
+# Parameters are set in the configuration file, here we define their positions,
+# which must be consistent with the config.
+outdir=${1}
+lstconfig=${2}
+tomlfile=${3}
+printf -v outfile_idx "%04d" ${4}
+kernel=${5}
+printf -v outfile_blchunk "%03d" ${6}
+isvalidation=${7}
+
+outname="lststack.${outfile_idx}.${outfile_blchunk}"
+
+runopts="--output-dir ${outdir} -k ${kernel} --toml ${tomlfile}"
+cfg="-o ${outname} --fileconf ${lstconfig} --fileidx ${outfile_idx} --blchunk ${outfile_blchunk}" 
+ 
+cmd="hnote run ${runopts} lststack ${cfg}"
+
+echo $cmd
+eval $cmd
+
+# If a marker file was saved indicating that plots were made, move the notebook to the 
+# appropriate directory so that it can be viewed on the web.
+if [ -f "${outdir}/${outname}.ipynb.hasplots" ]
+then
+    casename=$(basename $(builtin cd ${outdir}; pwd))
+    if [ ${isvalidation} -eq 1 ]; then
+        htmldir="/lustre/aoc/projects/hera/h6c-analysis/IDR2/notebooks/validation/lstbin/${casename}"
+    else
+        htmldir="/lustre/aoc/projects/hera/h6c-analysis/IDR2/notebooks/lstbin/${casename}"
+    fi
+    mkdir -p "${htmldir}"
+    cp "${outdir}/${outname}.html" "${htmldir}/"
+    rm "${outdir}/${outname}.ipynb.hasplots"
+fi
