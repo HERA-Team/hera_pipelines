8791b23b05177da4c20abef92f80e993bc6a99cf
Josh Dillon <jsdillon@berkeley.edu>
2025-03-27 16:26:40 -0700

    add round 3 flagging do script

--- pipelines/h6c/idr3/v1/analysis/task_scripts/do_FULL_DAY_RFI_ROUND_3_NOTEBOOK.sh
+++ pipelines/h6c/idr3/v1/analysis/task_scripts/do_FULL_DAY_RFI_ROUND_3_NOTEBOOK.sh
@@ -14,52 +14,52 @@ source ${src_dir}/_common.sh
 # 3 - nb_output_repo: repository for saving evaluated notebooks
 # 4+ - various settings
 fn=${1}
-# nb_template_dir=${2}
-# nb_output_repo=${3}
-# z_thresh=${4}
-# ws_z_thresh=${5}
-# avg_z_thresh=${6}
-# max_freq_flag_frac=${7}
-# max_time_flag_frac=${8}
-# avg_spectrum_filter_delay=${9}
-# eigenval_cutoff=${10}
-# time_avg_delay_filt_snr_thresh=${11}
-# time_avg_delay_filt_snr_dynamic_range=${12}
+nb_template_dir=${2}
+nb_output_repo=${3}
+min_samp_frac=${4}
+FM_low_freq=${5}
+FM_high_freq=${6}
+z_thresh=${7}
+ws_z_thresh=${8}
+avg_z_thresh=${9}
+max_freq_flag_frac=${10}
+max_time_flag_frac=${11}
+tv_chan_edges=${12}
+freq_conv_size=${13}
 
-# # Get JD from filename
-# jd=$(get_int_jd ${fn})
-# nb_outdir=${nb_output_repo}/full_day_rfi_round_2
-# nb_outfile=${nb_outdir}/full_day_rfi_round_2_${jd}.html
+# Get JD from filename
+jd=$(get_int_jd ${fn})
+nb_outdir=${nb_output_repo}/full_day_rfi_round_3
+nb_outfile=${nb_outdir}/full_day_rfi_round_3_${jd}.html
 
-# # Export variables used by the notebook
-# export SUM_FILE="$(cd "$(dirname "$fn")" && pwd)/$(basename "$fn")"
-# export SUM_SUFFIX="sum.uvh5"
-# export SMOOTH_CAL_SUFFIX="sum.smooth.calfits"
-# export ZSCORE_SUFFIX="sum.red_avg_zscore.h5"
-# export FLAG_WATERFALL2_SUFFIX="sum.flag_waterfall_round_2.h5"
-# export OUT_YAML_SUFFIX="_aposteriori_flags.yaml"
-# export Z_THRESH=${z_thresh}
-# export WS_Z_THRESH=${ws_z_thresh}
-# export AVG_Z_THRESH=${avg_z_thresh}
-# export MAX_FREQ_FLAG_FRAC=${max_freq_flag_frac}
-# export MAX_TIME_FLAG_FRAC=${max_time_flag_frac}
-# export AVG_SPECTRUM_FILTER_DELAY=${avg_spectrum_filter_delay}
-# export EIGENVAL_CUTOFF=${eigenval_cutoff}
-# export TIME_AVG_DELAY_FILT_SNR_THRESH=${time_avg_delay_filt_snr_thresh}
-# export TIME_AVG_DELAY_FILT_SNR_DYNAMIC_RANGE=${time_avg_delay_filt_snr_dynamic_range}
+# Export variables used by the notebook
+export SUM_FILE="$(cd "$(dirname "$fn")" && pwd)/$(basename "$fn")"
+export RED_AVG_FILE=${SUM_FILE%.sum.uvh5}.sum.smooth_calibrated.red_avg.uvh5
+export CORNER_TURN_MAP_YAML="$(cd "$(dirname "$fn")" && pwd)/single_baseline_files/corner_turn_map.yaml"
+export SNR_SUFFIX=".2Dfilt_SNR.uvh5"
+export OUTFILE="$(cd "$(dirname "$fn")" && pwd)/single_baseline_files/zen.${jd}.flag_waterfall_round_3.h5"
+export MIN_SAMP_FRAC=${min_samp_frac}
+export FM_LOW_FREQ=${FM_low_freq}
+export FM_HIGH_FREQ=${FM_high_freq}
+export Z_THRESH=${z_thresh}
+export WS_Z_THRESH=${ws_z_thresh}
+export AVG_Z_THRESH=${avg_z_thresh}
+export MAX_FREQ_FLAG_FRAC=${max_freq_flag_frac}
+export MAX_TIME_FLAG_FRAC=${max_time_flag_frac}
+export TV_CHAN_EDGES=${tv_chan_edges}
+export FREQ_CONV_SIZE=${freq_conv_size}
 
-# # Execute jupyter notebook and save as HTML
-# jupyter nbconvert --output=${nb_outfile} \
-# --to html \
-# --ExecutePreprocessor.timeout=-1 \
-# --execute ${nb_template_dir}/full_day_rfi_round_2.ipynb
-# echo Finished full-day rfi round 2 notebook at $(date)
+# Execute jupyter notebook and save as HTML
+jupyter nbconvert --output=${nb_outfile} \
+--to html \
+--ExecutePreprocessor.timeout=-1 \
+--execute ${nb_template_dir}/full_day_rfi_round_3.ipynb
+echo Finished full-day rfi round 3 notebook at $(date)
 
-# # Check to see that at least one output file was correctly produced
-# first_outfile=${SUM_FILE%.sum.uvh5}.sum.flag_waterfall_round_2.h5
-# if [ -f "$first_outfile" ]; then
-#     echo Resulting $first_outfile found.
-# else
-#     echo $first_outfile not produced.
-#     exit 1
-# fi
+# Check to see that OUTFILE was correctly produced
+if [ -f "$OUTFILE" ]; then
+    echo Resulting $OUTFILE found.
+else
+    echo $OUTFILE not produced.
+    exit 1
+fi
