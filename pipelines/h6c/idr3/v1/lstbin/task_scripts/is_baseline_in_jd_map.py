09c50edeb70b25c2b1739bf2def8f7cd73100254
Tyler Cox <tyler.a.cox@berkeley.edu>
2025-09-04 15:41:40 -0700

    Update import for LSTBinConfiguratorSingleBaseline

--- pipelines/h6c/idr3/v1/lstbin/task_scripts/is_baseline_in_jd_map.py
+++ pipelines/h6c/idr3/v1/lstbin/task_scripts/is_baseline_in_jd_map.py
@@ -4,6 +4,7 @@
 import argparse
 import numpy as np
 import toml
+from hera_cal import lst_stack
 
 # create an argparser 
 parser = argparse.ArgumentParser()
@@ -11,7 +12,7 @@ parser.add_argument("bl_str", help="the baseline string to use")
 parser.add_argument("toml_file", help="the path to the toml file")
 args = parser.parse_args()
 
-configurator = LSTBinConfiguratorSingleBaseline.from_toml(args.toml_file)
+configurator = lst_stack.config.LSTBinConfiguratorSingleBaseline.from_toml(args.toml_file)
 all_bl_strings = sorted(list(configurator.bl_to_file_map.keys()))
 
 bl_string_to_jd_map = {
