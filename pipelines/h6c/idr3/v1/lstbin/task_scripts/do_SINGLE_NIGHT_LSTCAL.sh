043a2d4e36f61cb2ec77ad8e9c1c5c1f26770bc7
Tyler Cox <tyler.a.cox@berkeley.edu>
2025-09-04 13:08:25 -0700

    Update LST calibration script for new notebook

--- pipelines/h6c/idr3/v1/lstbin/task_scripts/do_SINGLE_NIGHT_LSTCAL.sh
+++ pipelines/h6c/idr3/v1/lstbin/task_scripts/do_SINGLE_NIGHT_LSTCAL.sh
@@ -9,36 +9,42 @@ source ${src_dir}/_common.sh
 bl_str=${1}
 toml_file=${2}
 
-echo "I didn't run anything. I'm a placeholder!"
+# If Python exits 0, jd gets the printed value; else the 'else' branch runs.
+if jd="$(python3 "${src_dir}/is_baseline_in_jd_map.py" "$bl_str" "$toml_file")"; then
+  echo "Baseline $bl_str maps to JD $jd — proceeding..."
+  # ... do calibration work using "$jd" ...
+else
+  echo "Baseline $bl_str does not map to a JD. Exiting..."
+  exit 0
+fi
 
-# TODO: update
-# # read relevant variables from TOML
-# {
-#     IFS= read -r nb_template_dir
-#     IFS= read -r nb_output_repo
-#     IFS= read -r OUTDIR
-# } < <(
-#     python3 -c 'import toml
-# d = toml.load("'"$toml_file"'")
-# print(d["NOTEBOOK_OPTS"]["nb_template_dir"])
-# print(d["NOTEBOOK_OPTS"]["nb_output_repo"])
-# print(d["LST_STACK_OPTS"]["OUTDIR"])'
-# )
+# read relevant variables from TOML
+{
+    IFS= read -r nb_template_dir
+    IFS= read -r nb_output_repo
+    IFS= read -r OUTDIR
+} < <(
+    python3 -c 'import toml
+d = toml.load("'"$toml_file"'")
+print(d["NOTEBOOK_OPTS"]["nb_template_dir"])
+print(d["NOTEBOOK_OPTS"]["nb_output_repo"])
+print(d["LST_STACK_OPTS"]["OUTDIR"])'
+)
 
 # # export necessary environment variables to be read by notebook
-# export TOML_FILE=${toml_file}
-# export BASELINE_STRING=${bl_str}
-# echo Now running baseline ${BASELINE_STRING} with settings specified in ${TOML_FILE}
+export TOML_FILE=${toml_file}
+export BASELINE_STRING=${bl_str}
+echo Now running LST-cal on night ${jd} with settings specified in ${TOML_FILE}
 
 # # Execute jupyter notebook
-# nb_outfile="${OUTDIR}/baseline.${BASELINE_STRING}.lst_stack_and_reinpaint.html"
-# jupyter nbconvert --output=${nb_outfile} \
-# --to html \
-# --ExecutePreprocessor.timeout=-1 \
-# --execute ${nb_template_dir}/single_baseline_lst_stack_and_reinpaint.ipynb
-# echo Finished running single-baseline LST-stacking and re-inpainting notebook at $(date)
+nb_outfile="${OUTDIR}/zen.${jd}.single_night_lstcal.html"
+jupyter nbconvert --output=${nb_outfile} \
+--to html \
+--ExecutePreprocessor.timeout=-1 \
+--execute ${nb_template_dir}/single_night_lstcal.ipynb
+echo Finished running single-baseline LST-stacking and re-inpainting notebook at $(date)
 
-# # if baseline string is "0_4", copy notebook for easy viewing
-# if [[ ${BASELINE_STRING} == "0_4" ]]; then
-#     cp ${nb_outfile} ${nb_output_repo}/lststack/
-# fi
+# if baseline string is "0_0", copy notebook for easy viewing
+if [[ ${BASELINE_STRING} == "0_0" ]]; then
+    cp ${nb_outfile} ${nb_output_repo}/lststack/
+fi
