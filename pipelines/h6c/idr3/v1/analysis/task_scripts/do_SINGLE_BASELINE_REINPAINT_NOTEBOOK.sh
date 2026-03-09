fa9cd4dfc15758a2bed0dfe1f4f355b155776e72
Josh Dillon <jsdillon@berkeley.edu>
2026-03-09 09:54:44 -0700

    add reinpaint placeholder

---
+++ pipelines/h6c/idr3/v1/analysis/task_scripts/do_SINGLE_BASELINE_REINPAINT_NOTEBOOK.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+set -e
+
+# Placeholder for re-inpainting single baselines using updated flags.
+# This notebook is not yet implemented.
+
+echo "Re-inpainting notebook is not yet implemented."
+exit 0
