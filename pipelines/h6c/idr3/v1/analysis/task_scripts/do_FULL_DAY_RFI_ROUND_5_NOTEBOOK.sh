3710791cb0464f0ec64dbf0472e17e920654996b
Josh Dillon <jsdillon@berkeley.edu>
2026-03-09 09:54:52 -0700

    add round 5 placeholder

---
+++ pipelines/h6c/idr3/v1/analysis/task_scripts/do_FULL_DAY_RFI_ROUND_5_NOTEBOOK.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+set -e
+
+# Placeholder for fifth-round full-day RFI flagging notebook.
+# This notebook is not yet implemented.
+
+echo "Round 5 RFI flagging notebook is not yet implemented."
+exit 0
